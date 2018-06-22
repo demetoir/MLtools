6ef79c6955502c70b6ba492beccf805e0ffaafc7
demetoir <wnsqlehlswk@naver.com>
2018-06-23 02:21:54 +0900

    add cloning obj to cast class casting
    to better code complete

--- util/MixIn.py
+++ util/MixIn.py
@@ -12,6 +12,11 @@ class PickleMixIn:
     def dump(self, path):
         dump_pickle(self, path)
 
-    @staticmethod
-    def load(path):
-        return load_pickle(path)
+    def load(self, path):
+        load_obj = load_pickle(path)
+        if load_obj.__class__ is not self.__class__:
+            raise TypeError(f"load obj is not {load_obj.__class__} is not match with expected class {self.__class__}")
+        new_obj = self.__class__()
+        for key, items in load_obj.__dict__.items():
+            setattr(new_obj, key, items)
+        return new_obj
