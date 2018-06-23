2437a339d0f945a8e3dad5e2441cee96dccd92d4
demetoir <wnsqlehlswk@naver.com>
2018-06-23 17:42:09 +0900

    add exception handling

--- sklearn_like_toolkit/base/MixIn.py
+++ sklearn_like_toolkit/base/MixIn.py
@@ -29,10 +29,14 @@ class clf_metric_MixIn:
         return self._metrics[metric](Y_true, Y_predict)
 
     def _apply_metric_pack(self, Y_true, Y_predict):
-        return {
-            key: self._apply_metric(Y_true, Y_predict, key)
-            for key in self._metrics
-        }
+        ret = {}
+        for key in self._metrics:
+            try:
+                ret[key] = self._apply_metric(Y_true, Y_predict, key)
+            except BaseException as e:
+                # log_error_trace(self.log.warn, e)
+                pass
+        return ret
 
 
 class DummyParamMixIN():
