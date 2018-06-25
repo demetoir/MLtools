267e4da38bebf2f26b7b47129eaa36e625f36a2b
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-25 18:39:13 +0900

    add test_python_syntax.py

---
+++ unit_test/test_python_syntax.py
@@ -0,0 +1,35 @@
+def test_metaclass_mixIn():
+    class meta_B(type):
+
+        def __init__(cls, name, bases, cls_dict):
+            type.__init__(cls, name, bases, cls_dict)
+            print('meta_B')
+
+    class meta_A(type):
+
+        def __init__(cls, name, bases, cls_dict):
+            type.__init__(cls, name, bases, cls_dict)
+            print('meta_A')
+
+    class A(metaclass=meta_A):
+        def __init__(self):
+            print('class_A')
+
+    class B(metaclass=meta_B):
+        def __init__(self):
+            print('class_B')
+
+    class meta_AB(meta_A, meta_B):
+        def __init__(cls, name, bases, cls_dict):
+            meta_A.__init__(cls, name, bases, cls_dict)
+            meta_B.__init__(cls, name, bases, cls_dict)
+            print('meta_AB')
+
+        pass
+
+    class C(A, B, metaclass=meta_AB):
+        def __init__(self):
+            A.__init__(self)
+            B.__init__(self)
+
+    C()
