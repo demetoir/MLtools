3047baf8eea1eb2b816c402471e8aa4ed95b52ea
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-26 04:25:29 +0900

    better testing code

--- unit_test/sklearn_like_toolkit/test_WrapperClfPack.py
+++ unit_test/sklearn_like_toolkit/test_WrapperClfPack.py
@@ -1,5 +1,4 @@
-from pprint import pprint
-from script.sklearn_like_toolkit.warpper.sklearn_wrapper import skMLP, skBernoulli_NB
+from script.sklearn_like_toolkit.warpper.sklearn_wrapper import skBernoulli_NB
 from script.sklearn_like_toolkit.wrapperGridSearchCV import wrapperGridSearchCV
 from script.data_handler.DatasetPackLoader import DatasetPackLoader
 from script.sklearn_like_toolkit.ClassifierPack import ClassifierPack
@@ -18,17 +17,16 @@ def test_param_search():
     clf_pack.param_search(train_Xs, train_Ys)
     train_score = clf_pack.score(train_Xs, train_Ys)
     valid_score = clf_pack.score(valid_Xs, valid_Ys)
-
-    pprint(train_score)
-    pprint(valid_score)
+    print(train_score)
+    print(valid_score)
 
     path = clf_pack.save_params()
     clf_pack.load_params(path)
     clf_pack.fit(train_Xs, train_Ys)
     train_score = clf_pack.score(train_Xs, train_Ys)
     valid_score = clf_pack.score(valid_Xs, valid_Ys)
-    pprint(train_score)
-    pprint(valid_score)
+    print(train_score)
+    print(valid_score)
 
 
 def test_make_FoldingHardVote():
@@ -50,7 +48,7 @@ def test_make_FoldingHardVote():
 
 
 def test_make_stackingClf():
-    clf = ClassifierPack()
+    clf = ClassifierPack(['skMLP', "skBernoulli_NB", "skDecisionTree"])
 
     clf = clf.make_stackingClf(meta_clf)
     clf.fit(train_Xs, train_Ys)
@@ -66,7 +64,7 @@ def test_make_stackingClf():
 
 
 def test_make_stackingCVClf():
-    clf = ClassifierPack()
+    clf = ClassifierPack(['skMLP', "skBernoulli_NB", "skDecisionTree"])
     meta_clf = clf.pack["skBernoulli_NB"]
     clf = clf.make_stackingCVClf(meta_clf)
     clf.fit(train_Xs, train_Ys)
@@ -82,18 +80,18 @@ def test_make_stackingCVClf():
 
 
 def test_ClassifierPack():
-    clf = ClassifierPack()
+    clf = ClassifierPack(['skMLP', "skBernoulli_NB", "skDecisionTree"])
     clf.fit(train_Xs, train_Ys)
     predict = clf.predict(valid_Xs[:2])
-    pprint('predict', predict)
+    print('predict', predict)
     proba = clf.predict_proba(valid_Xs[:2])
-    pprint('predict_proba', proba)
+    print('predict_proba', proba)
 
     score = clf.score(valid_Xs, valid_Ys)
-    pprint('test score', score)
+    print('test score', score)
 
     score_pack = clf.score_pack(valid_Xs, valid_Ys)
-    pprint('score pack', score_pack)
+    print('score pack', score_pack)
 
 
 def test_pickle_clf_pack():
@@ -101,49 +99,49 @@ def test_pickle_clf_pack():
     clf_pack.fit(train_Xs, train_Ys)
 
     score = clf_pack.score_pack(train_Xs, train_Ys)
-    pprint(score)
+    print(score)
     clf_pack.dump('./test_pickle_clf_pack.pkl')
 
     clf_pack = clf_pack.load('./test_pickle_clf_pack.pkl')
     score = clf_pack.score_pack(train_Xs, train_Ys)
-    pprint(score)
+    print(score)
 
 
 def test_wrapperGridSearchCV():
-    clf_cls = skMLP
+    clf_cls = skBernoulli_NB
     base_clf = clf_cls()
     param_search = wrapperGridSearchCV(base_clf, clf_cls.tuning_grid)
 
     param_search.fit(train_Xs, train_Ys)
     result = param_search.cv_results_
-    pprint(result)
+    print(result)
 
     best_clf = param_search.best_estimator_
-    pprint(best_clf)
+    print(best_clf)
 
     score = best_clf.score(valid_Xs, valid_Ys)
-    pprint(score)
+    print(score)
 
     score = best_clf.score_pack(valid_Xs, valid_Ys)
-    pprint(score)
+    print(score)
 
 
 def test_wrapper_pack_grid_search():
     path = './test_wrapper_pack_grid_search.pkl'
-    clf_pack = ClassifierPack()
+    clf_pack = ClassifierPack(['skMLP', "skBernoulli_NB", "skDecisionTree"])
     clf_pack.fit(train_Xs, train_Ys)
     clf_pack.gridSearchCV(train_Xs, train_Ys)
     score = clf_pack.score_pack(train_Xs, train_Ys)
-    pprint(score)
+    print(score)
     clf_pack.dump(path)
 
     clf_pack = ClassifierPack().load(path)
     score = clf_pack.score_pack(train_Xs, train_Ys)
-    pprint(score)
+    print(score)
     score = clf_pack.score_pack(valid_Xs, valid_Ys)
-    pprint(score)
+    print(score)
 
     score = clf_pack.score(valid_Xs, valid_Ys)
-    pprint(score)
+    print(score)
     result = clf_pack.optimize_result
-    pprint(result)
+    print(result)
