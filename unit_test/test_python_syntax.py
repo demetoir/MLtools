031393d59c27e644343946479f514596d7f3c35a
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-26 02:42:34 +0900

    add test

--- unit_test/test_python_syntax.py
+++ unit_test/test_python_syntax.py
@@ -37,3 +37,16 @@ def test_metaclass_mixIn():
     B()
     C()
 
+
+def test_getitem():
+    class cls:
+        def __init__(self):
+            self.pack = [i for i in range(10)]
+
+        def __getitem__(self, item):
+            return self.pack.__getitem__(item)
+
+    c = cls()
+    for i in range(10):
+        print(c[i])
+    print(c[3:5])
