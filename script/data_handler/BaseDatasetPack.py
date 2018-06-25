3e6c602031ac2f0a05af056145f38bb3edf7df66
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-26 02:42:23 +0900

    rename attr, add __getitem__

--- script/data_handler/BaseDatasetPack.py
+++ script/data_handler/BaseDatasetPack.py
@@ -5,47 +5,50 @@ class BaseDatasetPack:
     def __init__(self, caching=True, **kwargs):
         self.logger = Logger(self.__class__.__name__)
         self.log = self.logger.get_log()
-        self.set = {}
+        self.pack = {}
         self.caching = caching
 
     def load(self, path, **kwargs):
-        for k in self.set:
-            self.set[k].load(path, **kwargs)
+        for k in self.pack:
+            self.pack[k].load(path, **kwargs)
 
     def shuffle(self):
-        for key in self.set:
-            self.set[key].shuffle()
+        for key in self.pack:
+            self.pack[key].shuffle()
 
     def split(self, from_key, a_key, b_key, rate):
-        from_set = self.set[from_key]
-        self.set.pop(from_key)
+        from_set = self.pack[from_key]
+        self.pack.pop(from_key)
 
         a_set, b_set = from_set.split(rate)
-        self.set[a_key] = a_set
-        self.set[b_key] = b_set
+        self.pack[a_key] = a_set
+        self.pack[b_key] = b_set
         return a_set, b_set
 
     def merge_shuffle(self, a_key, b_key, rate):
-        a_set = self.set[a_key]
-        b_set = self.set[b_key]
+        a_set = self.pack[a_key]
+        b_set = self.pack[b_key]
 
         merge_set = a_set.merge(a_set, b_set)
         merge_set.shuffle()
         a_set, b_set = merge_set.split(rate, shuffle=True)
 
-        self.set[a_key] = a_set
-        self.set[b_key] = b_set
+        self.pack[a_key] = a_set
+        self.pack[b_key] = b_set
         return a_set, b_set
 
     def merge(self, a_key, b_key, merge_set_key):
-        a_set = self.set[a_key]
-        b_set = self.set[b_key]
-        self.set.pop(a_key)
-        self.set.pop(b_key)
+        a_set = self.pack[a_key]
+        b_set = self.pack[b_key]
+        self.pack.pop(a_key)
+        self.pack.pop(b_key)
 
         merge_set = a_set.merge(a_set, b_set)
-        self.set[merge_set_key] = merge_set
+        self.pack[merge_set_key] = merge_set
 
     def sort(self, sort_key=None):
-        for key in self.set:
-            self.set[key].sort(sort_key)
+        for key in self.pack:
+            self.pack[key].sort(sort_key)
+
+    def __getitem__(self, item):
+        return self.pack.__getitem__(item)
