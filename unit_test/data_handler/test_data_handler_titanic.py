8df4192ae50f2ce0b20ff50f70ca3430ed956d7c
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-26 04:25:46 +0900

    add test_code

--- unit_test/data_handler/test_data_handler_titanic.py
+++ unit_test/data_handler/test_data_handler_titanic.py
@@ -1,3 +1,4 @@
+from script.data_handler.DatasetPackLoader import DatasetPackLoader
 from script.data_handler.titanic import trans_cabin, load_merge_set
 
 
@@ -9,3 +10,15 @@ def test_trans_cabin():
     # pprint(head)
     info = cabin.info()
     # pprint(info)
+
+
+def test_titanic_pack():
+    datapack = DatasetPackLoader().load_dataset("titanic")
+    train_set = datapack['train']
+
+    train_set, valid_set = train_set.split((7, 3))
+    train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
+    valid_Xs, valid_Ys = valid_set.full_batch(['Xs', 'Ys'])
+    sample_Xs, sample_Ys = valid_Xs[:2], valid_Ys[:2]
+    print(train_Xs)
+
