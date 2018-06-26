b2a80ea75d0ca77fc3c98deb61626b2e41b47632
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-27 05:36:49 +0900

    update bench_code.py

--- script/workbench/bench_code.py
+++ script/workbench/bench_code.py
@@ -1,9 +1,9 @@
 # -*- coding:utf-8 -*-
+from script.model.sklearn_like_model.MLPClassifier import MLPClassifier
 from script.data_handler.DatasetPackLoader import DatasetPackLoader
 from script.model.sklearn_like_model.AE.AutoEncoder import AutoEncoder
-# from script.sklearn_like_toolkit.ClassifierPack import ClassifierPack
+from script.sklearn_like_toolkit.ClassifierPack import ClassifierPack
 from script.model.sklearn_like_model.GAN.GAN import GAN
-
 from script.util.Logger import pprint_logger, Logger
 import numpy as np
 from script.util.deco import deco_timeit
@@ -34,8 +34,7 @@ def finger_print(size, head='_'):
     return ret
 
 
-@deco_timeit
-def main():
+def test_GAN():
     class_ = GAN
     data_pack = DatasetPackLoader().load_dataset("titanic")
     dataset = data_pack['train']
@@ -43,34 +42,120 @@ def main():
     sample_X = Xs[:2]
     sample_Y = Ys[:2]
 
-    # model = class_(dataset.input_shapes)
-    # model.build()
-    #
-    # model.train(Xs, epoch=1)
-    #
-    # metric = model.metric(sample_X)
-    # print(metric)
-    #
-    # gen = model.generate(size=2)
-    # print(gen)
-    #
-    # path = model.save()
-    # path = """C:\\Users\\demetoir_desktop\\PycharmProjects\\MLtools\\instance\\demetoir_GAN_2018-06-26_06-52-36"""
-    path = None
+    model = class_()
+    model.train(Xs, epoch=1)
+
+    metric = model.metric(sample_X)
+    print(metric)
+
+    gen = model.generate(size=2)
+    print(gen)
+
+    path = model.save()
+
     model = class_()
     model.load(path)
     print('model reloaded')
 
-    for i in range(100):
+    for i in range(2):
         model.train(Xs, epoch=1)
-
-    gen = model.generate(size=1)
-
-    gen[gen < 0.5] = 0
-    gen[gen > 0.5] = 1
-    print(gen)
 
     metric = model.metric(sample_X)
     print(metric)
 
     model.save()
+
+
+def test_AE():
+    class_ = AutoEncoder
+    data_pack = DatasetPackLoader().load_dataset("titanic")
+    dataset = data_pack['train']
+    Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
+    sample_X = Xs[:2]
+    sample_Y = Ys[:2]
+
+    model = class_()
+    model.train(Xs, epoch=1)
+
+    metric = model.metric(sample_X)
+    print(metric)
+
+    code = model.code(sample_X)
+    print(code)
+
+    recon = model.recon(sample_X)
+    print(recon)
+
+    path = model.save()
+
+    model = class_()
+    model.load(path)
+    print('model reloaded')
+
+    for i in range(2):
+        model.train(Xs, epoch=1)
+
+    metric = model.metric(sample_X)
+    print(metric)
+
+    metric = model.metric(sample_X)
+    print(metric)
+
+    code = model.code(sample_X)
+    print(code)
+
+    recon = model.recon(sample_X)
+    print(recon)
+
+    model.save()
+
+
+def test_CLf():
+    class_ = MLPClassifier
+    data_pack = DatasetPackLoader().load_dataset("titanic")
+    dataset = data_pack['train']
+    Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
+    sample_X = Xs[:2]
+    sample_Y = Ys[:2]
+
+    model = class_()
+    model.train(Xs, Ys)
+
+    predict = model.predict(sample_X)
+    print(predict)
+
+    score = model.score(Xs, Ys)
+    print(score)
+
+    proba = model.proba(sample_X)
+    print(proba)
+
+    metric = model.metric(sample_X, sample_Y)
+    print(metric)
+
+    path = model.save()
+
+    model = class_()
+    model.load(path)
+    model.train(Xs, Ys)
+
+    predict = model.predict(sample_X)
+    print(predict)
+
+    score = model.score(Xs, Ys)
+    print(score)
+
+    proba = model.proba(sample_X)
+    print(proba)
+
+    metric = model.metric(sample_X, sample_Y)
+    print(metric)
+
+    model.save()
+
+
+@deco_timeit
+def main():
+    test_CLf()
+    # test_GAN()
+    # test_AE()
