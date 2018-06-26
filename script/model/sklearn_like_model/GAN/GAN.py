505a70a0e734a2e0a653d5a1d80edfd0bb307be8
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-27 04:42:00 +0900

    remake GAN.py

--- script/model/sklearn_like_model/GAN/GAN.py
+++ script/model/sklearn_like_model/GAN/GAN.py
@@ -1,10 +1,9 @@
-from functools import reduce
 from script.model.sklearn_like_model.BaseModel import BaseModel
-from script.model.sklearn_like_model.DummyDataset import DummyDataset
 from script.util.Stacker import Stacker
 from script.util.tensor_ops import *
+from functools import reduce
+from tqdm import trange
 import numpy as np
-import tensorflow as tf
 
 
 class TrainFailError(BaseException):
@@ -15,75 +14,65 @@ class InputShapeError(BaseException):
     pass
 
 
-class GAN(BaseModel):
-    def hyper_param_key(self):
-        return [
-            'n_noise',
-            'batch_size',
-            'learning_rate',
-            'D_net_shape',
-            'G_net_shape',
-        ]
+class basicGANPropertyMixIN:
 
     @property
     def _Xs(self):
-        return self.Xs
+        return getattr(self, 'Xs', None)
 
     @property
     def _zs(self):
-        return self.zs
+        return getattr(self, 'zs', None)
 
     @property
     def _train_ops(self):
-        return [self.train_G, self.train_D, self.op_inc_global_step]
+        return [
+            getattr(self, 'train_G'),
+            getattr(self, 'train_D'),
+            getattr(self, 'op_inc_global_step')
+        ]
 
     @property
     def _metric_ops(self):
-        return [self.D_loss, self.G_loss]
+        return [
+            self._loss_D,
+            self._loss_G
+        ]
 
     @property
     def _loss_D(self):
-        return self.D_loss
+        return getattr(self, 'D_loss')
 
     @property
     def _loss_G(self):
-        return self.G_loss
+        return getattr(self, 'G_loss')
 
     @property
     def _gen_ops(self):
-        return self.Xs_gen
+        return getattr(self, 'Xs_gen', None)
 
-    def hyper_parameter(self):
-        self.n_noise = 256
-        self.batch_size = 64
-        self.learning_rate = 0.0002
-        self.D_net_shape = (128, 128)
-        self.G_net_shape = (128, 128)
-        self.D_learning_rate = 0.0001
-        self.G_learning_rate = 0.0001
 
-    def build_input_shapes(self, input_shapes):
-        self.X_batch_key = 'Xs'
-        self.X_shape = input_shapes[self.X_batch_key]
-        self.Xs_shape = [None] + self.X_shape
-        self.X_flatten_size = reduce(lambda x, y: x * y, self.X_shape)
+def flatten_shape(x):
+    return reduce(lambda a, b: a * b, x)
 
-        self.z_shape = [self.n_noise]
-        self.zs_shape = [None, self.n_noise]
 
-    def generator(self, z, net_shapes, reuse=False, name='generator'):
+class basicGeneratorMixIn:
+    @staticmethod
+    def generator(z, net_shapes, flatten_size, output_shape, reuse=False, name='generator'):
         with tf.variable_scope(name, reuse=reuse):
             layer = Stacker(z)
 
             for shape in net_shapes:
                 layer.linear(shape)
 
-            layer.linear(self.X_flatten_size)
+            layer.linear(flatten_size)
             layer.sigmoid()
-            layer.reshape(self.Xs_shape)
+            layer.reshape(output_shape)
 
         return layer.last_layer
 
+
+class basicDiscriminatorMixIn:
     @staticmethod
     def discriminator(X, net_shapes, reuse=False, name='discriminator'):
         with tf.variable_scope(name, reuse=reuse):
@@ -97,19 +86,76 @@ class GAN(BaseModel):
 
         return layer.last_layer
 
-    def build_main_graph(self):
+
+class GAN_z_noiseMixIn:
+    @staticmethod
+    def get_z_noise(shape):
+        return np.random.uniform(-1.0, 1.0, size=shape)
+
+
+class GAN(BaseModel, basicGANPropertyMixIN, basicGeneratorMixIn, basicDiscriminatorMixIn, GAN_z_noiseMixIn):
+    _input_shape_keys = [
+        'X_shape',
+        'Xs_shape',
+        'X_flatten_size',
+        'z_shape',
+        'zs_shape',
+    ]
+    _params_keys = [
+        'n_noise',
+        'batch_size',
+        'learning_rate',
+        'D_net_shape',
+        'G_net_shape',
+    ]
+
+    def __init__(self, verbose=10, **kwargs):
+        BaseModel.__init__(self, verbose, **kwargs)
+        self.n_noise = 256
+        self.batch_size = 64
+        self.learning_rate = 0.0002
+        self.D_net_shape = (128, 128)
+        self.G_net_shape = (128, 128)
+        self.D_learning_rate = 0.0001
+        self.G_learning_rate = 0.0001
+
+        self.X_shape = None
+        self.Xs_shape = None
+        self.X_flatten_size = None
+        self.z_shape = None
+        self.zs_shape = None
+
+    def build_input_shapes(self, shapes):
+        X_shape = shapes['Xs']
+        Xs_shape = [None] + list(X_shape)
+
+        X_flatten_size = flatten_shape(X_shape)
+
+        z_shape = [self.n_noise]
+        zs_shape = [None, self.n_noise]
+
+        ret = {
+            'X_shape': X_shape,
+            'Xs_shape': Xs_shape,
+            'X_flatten_size': X_flatten_size,
+            'z_shape': z_shape,
+            'zs_shape': zs_shape
+        }
+        return ret
+
+    def _build_main_graph(self):
         self.Xs = placeholder(tf.float32, self.Xs_shape, "Xs")
         self.zs = placeholder(tf.float32, self.zs_shape, "zs")
 
-        self.G = self.generator(self.zs, self.G_net_shape)
-        self.Xs_gen = tf.identity(self.G, 'Xs_gen')
+        self.G = self.generator(self.zs, self.G_net_shape, self.X_flatten_size, self.Xs_shape)
+        self.Xs_gen = identity(self.G, 'Xs_gen')
         self.D_real = self.discriminator(self.Xs, self.D_net_shape)
         self.D_gen = self.discriminator(self.Xs_gen, self.D_net_shape, reuse=True)
 
         self.G_vals = collect_vars(join_scope(get_scope(), 'generator'))
         self.D_vals = collect_vars(join_scope(get_scope(), 'discriminator'))
 
-    def build_loss_function(self):
+    def _build_loss_function(self):
         self.D_real_loss = tf.identity(self.D_real, 'loss_D_real')
         self.D_gen_loss = tf.identity(self.D_gen, 'loss_D_gen')
 
@@ -118,22 +164,19 @@ class GAN(BaseModel):
         self.D_loss_mean = tf.reduce_mean(self.D_loss)
         self.G_loss_mean = tf.reduce_mean(self.G_loss)
 
-    def build_train_ops(self):
+    def _build_train_ops(self):
         self.train_D = tf.train.AdamOptimizer(learning_rate=self.D_learning_rate) \
             .minimize(self.D_loss, var_list=self.D_vals)
 
         self.train_G = tf.train.AdamOptimizer(learning_rate=self.G_learning_rate) \
             .minimize(self.G_loss, var_list=self.G_vals)
 
-    @staticmethod
-    def get_z_noise(shape):
-        return np.random.uniform(-1.0, 1.0, size=shape)
-
     def train(self, Xs, epoch=1, save_interval=None, batch_size=None, shuffle=True):
-        self.if_not_ready_to_train()
+        shapes = {'Xs': Xs.shape[1:]}
+        self._apply_input_shapes(self.build_input_shapes(shapes))
+        self.is_built()
 
-        dataset = DummyDataset()
-        dataset.add_data('Xs', Xs)
+        dataset = self.to_dummyDataset(Xs=Xs)
 
         if batch_size is None:
             batch_size = self.batch_size
@@ -141,8 +184,7 @@ class GAN(BaseModel):
         iter_num = 0
         iter_per_epoch = dataset.size // batch_size
         self.log.info("train epoch {}, iter/epoch {}".format(epoch, iter_per_epoch))
-
-        for e in range(epoch):
+        for e in trange(epoch):
             if shuffle:
                 dataset.shuffle()
 
@@ -173,9 +215,9 @@ class GAN(BaseModel):
 
     def generate(self, size):
         zs = self.get_z_noise([size, self.n_noise])
-        return self.sess.run(self._gen_ops, feed_dict={self.zs: zs})
+        return self.get_tf_values(self._gen_ops, {self.zs: zs})
 
     def metric(self, Xs):
         zs = self.get_z_noise([Xs.shape[0], self.n_noise])
-        D_loss, G_loss = self.sess.run(self._metric_ops, feed_dict={self._Xs: Xs, self._zs: zs})
+        D_loss, G_loss = self.get_tf_values(self._metric_ops, {self._Xs: Xs, self._zs: zs})
         return {'D_loss': D_loss, 'G_loss': G_loss}
