83cf3f8522ef79eba2289413fce4d01d28c06dd7
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-27 04:41:42 +0900

    remake BaseModel.py

--- script/model/sklearn_like_model/BaseModel.py
+++ script/model/sklearn_like_model/BaseModel.py
@@ -1,49 +1,10 @@
-from script.dict_keys.model_metadata_keys import *
-from script.util.Logger import Logger
-from script.util.misc_util import dump_json, load_json, time_stamp, setup_directory
+from script.data_handler.DummyDataset import DummyDataset
+from script.util.MixIn import LoggerMixIn
+from script.util.misc_util import dump_json, load_json, time_stamp, path_join, log_error_trace
+from script.util.misc_util import setup_directory
 from env_settting import *
 import tensorflow as tf
 import os
-import sys
-import traceback
-
-
-def deco_exception_handle(func):
-    """decorator for catch exception and log"""
-
-    def wrapper(*args, **kwargs):
-        self = args[0]
-        log_func = self.log
-        try:
-            return func(*args, **kwargs)
-        except KeyboardInterrupt:
-            log_func("KeyboardInterrupt detected abort process")
-        except Exception as e:
-            log_error_trace(log_func, e)
-
-    return wrapper
-
-
-def log_error_trace(log_func, e, head=""):
-    exc_type, exc_value, exc_traceback = sys.exc_info()
-
-    msg = '%s\n %s %s : %s \n' % (
-        head,
-        "".join(traceback.format_tb(exc_traceback)),
-        e.__class__.__name__,
-        e,
-    )
-    log_func(msg)
-
-
-def deco_logging_func_name(func):
-    def wrapper(*args, **kwargs):
-        self = args[0]
-        log_func = self.log
-        log_func(func.__name__)
-        return func(*args, **kwargs)
-
-    return wrapper
 
 
 class ModelBuildFailError(BaseException):
@@ -81,168 +42,194 @@ class MetaBaseModel(type):
 
 
 META_DATA_FILE_NAME = 'instance.meta'
+meta_json = 'meta.json'
+params_json = 'params.json'
+input_shapes_json = 'input_shapes.json'
 INSTANCE_FOLDER = 'instance'
-VISUAL_RESULT_FOLDER = 'visual_results'
 
 
-class BaseModel:
+class input_shapesMixIN:
+    _input_shape_keys = [
+        'X_shape',
+        'Xs_shape',
+        'X_flatten_size',
+        'z_shape',
+        'zs_shape',
+    ]
+
+    def __init__(self):
+        for key in self._input_shape_keys:
+            setattr(self, key, None)
+
+    @property
+    def input_shapes(self):
+        return self._collect_input_shapes()
+
+    def _apply_input_shapes(self, input_shapes):
+        for key in self._input_shape_keys:
+            setattr(self, key, input_shapes[key])
+
+    def _collect_input_shapes(self):
+        input_shapes = {}
+        for key in self._input_shape_keys:
+            input_shapes[key] = getattr(self, key, None)
+        return input_shapes
+
+    @staticmethod
+    def _check_input_shapes(a, b):
+        return True if dict(a) == dict(b) else False
+
+    def _load_input_shapes(self, path):
+        self._apply_input_shapes(load_json(path))
+
+    def _save_input_shapes(self, path):
+        dump_json(self.input_shapes, path)
+
+
+class metadataMixIN:
+    _metadata_keys = [
+        'id',
+        'instance_path',
+        'metadata_path',
+        'check_point_path',
+        'save_folder_path',
+    ]
+
+    def __init__(self):
+        for key in self._metadata_keys:
+            setattr(self, key, None)
+
+    @property
+    def metadata(self):
+        return self._collect_metadata()
+
+    def _collect_metadata(self):
+        metadata = {}
+        for key in self._metadata_keys:
+            metadata[key] = getattr(self, key, None)
+        return metadata
+
+    def _apply_metadata(self, metadata):
+        for key in self._metadata_keys:
+            setattr(self, key, metadata[key])
+
+    def _load_metadata(self, path):
+        self._apply_metadata(load_json(path))
+
+    def _save_metadata(self, path):
+        dump_json(self.metadata, path)
+
+
+class paramsMixIn:
+    _params_keys = []
+
+    def __init__(self):
+        for key in self._params_keys:
+            setattr(self, key, None)
+
+    @property
+    def params(self):
+        return self._collect_params()
+
+    def _collect_params(self):
+        params = {}
+        for key in self._params_keys:
+            params[key] = getattr(self, key, None)
+        return params
+
+    def _apply_params(self, params):
+        for key in self._params_keys:
+            setattr(self, key, params[key])
+
+    def _load_params(self, path):
+        self._apply_params(load_json(path))
+
+    def _save_params(self, path):
+        dump_json(self.params, path)
+
+
+class BaseModel(LoggerMixIn, input_shapesMixIN, metadataMixIN, paramsMixIn):
     """Abstract class of model for tensorflow graph"""
     AUTHOR = 'demetoir'
 
-    def __str__(self):
-        return "%s_%s" % (self.AUTHOR, self.__class__.__name__)
-
-    def __init__(self, input_shapes=None, params=None, logger_path=None, root_path=ROOT_PATH):
+    def __init__(self, verbose=10, **kwargs):
         """create instance of AbstractModel
 
+        :param verbose:
         :type logger_path: str
         :param logger_path: path for log file
         if logger_path is None, log ony stdout
         """
-        self.root_path = root_path
+        input_shapesMixIN.__init__(self)
+        metadataMixIN.__init__(self)
+        paramsMixIn.__init__(self)
 
-        if logger_path is None:
-            self.log = Logger(self.__class__.__name__, LOG_PATH)
-        else:
-            self.log = Logger(self.__class__.__name__, logger_path)
-
+        self.verbose = verbose
         self.sess = None
         self.saver = None
-        self.summary_writer = None
-        self.is_built = False
+        self._is_built = False
 
         # gen instance id
-        self.input_shapes = input_shapes
-        self.params = params
-
         self.id = "_".join([self.__str__(), time_stamp()])
-        self.instance_path = os.path.join(INSTANCE_PATH, self.id)
-        self.instance_visual_result_folder_path = os.path.join(self.instance_path, VISUAL_RESULT_FOLDER)
-        self.instance_source_folder_path = os.path.join(self.instance_path, 'src_code')
-        self.instance_summary_folder_path = os.path.join(self.instance_path, 'summary')
-        self.instance_class_name = self.__class__.__name__
-        self.instance_source_path = os.path.join(self.instance_source_folder_path, self.id + '.py')
-        self.metadata_path = os.path.join(self.instance_path, 'instance.meta')
-        self.save_folder_path = os.path.join(self.instance_path, 'check_point')
-        self.check_point_path = os.path.join(self.save_folder_path, 'instance.ckpt')
 
-        self.metadata = {
-            MODEL_METADATA_KEY_INSTANCE_ID: self.id,
-            MODEL_METADATA_KEY_INSTANCE_PATH: self.instance_path,
-            MODEL_METADATA_KEY_INSTANCE_VISUAL_RESULT_FOLDER_PATH: self.instance_visual_result_folder_path,
-            MODEL_METADATA_KEY_INSTANCE_SOURCE_FOLDER_PATH: self.instance_source_folder_path,
-            MODEL_METADATA_KEY_INSTANCE_SOURCE_PATH: self.instance_source_path,
-            MODEL_METADATA_KEY_INSTANCE_SUMMARY_FOLDER_PATH: self.instance_summary_folder_path,
-            MODEL_METADATA_KEY_INSTANCE_CLASS_NAME: self.instance_class_name,
-            MODEL_METADATA_KEY_METADATA_PATH: self.metadata_path,
-            MODEL_METADATA_KEY_CHECK_POINT_PATH: self.check_point_path,
-            MODEL_METADATA_KEY_SAVE_FOLDER_PATH: self.save_folder_path,
-            MODEL_METADATA_KEY_PARAMS: self.params,
-            MODEL_METADATA_KEY_INPUT_SHAPES: self.input_shapes,
-        }
+    def __str__(self):
+        return "%s_%s" % (self.AUTHOR, self.__class__.__name__)
 
     def __del__(self):
         # TODO this del need hack
         try:
-            self.close_session()
+            self._close_session()
+            self._close_saver()
             # reset tensorflow graph
             tf.reset_default_graph()
 
-            del self.sess
-            del self.root_path
-            del self.log
         except BaseException:
             pass
 
-    @property
-    def hyper_param_key(self):
-        return []
-
-    def setup_model(self):
-        self.log.debug('init directory')
-        setup_directory(self.instance_path)
-        setup_directory(self.instance_visual_result_folder_path)
-        setup_directory(self.instance_source_folder_path)
-        setup_directory(self.instance_summary_folder_path)
-        setup_directory(self.save_folder_path)
-
-    def load_metadata(self, path):
-        self.metadata = load_json(path)
-
-        self.id = self.metadata[MODEL_METADATA_KEY_INSTANCE_ID]
-        self.instance_path = self.metadata[MODEL_METADATA_KEY_INSTANCE_PATH]
-        self.instance_visual_result_folder_path = self.metadata[MODEL_METADATA_KEY_INSTANCE_VISUAL_RESULT_FOLDER_PATH]
-        self.instance_source_path = self.metadata[MODEL_METADATA_KEY_INSTANCE_SOURCE_PATH]
-        self.instance_class_name = self.metadata[MODEL_METADATA_KEY_INSTANCE_CLASS_NAME]
-        self.instance_summary_folder_path = self.metadata[MODEL_METADATA_KEY_INSTANCE_SUMMARY_FOLDER_PATH]
-        self.save_folder_path = self.metadata[MODEL_METADATA_KEY_SAVE_FOLDER_PATH]
-        self.check_point_path = self.metadata[MODEL_METADATA_KEY_CHECK_POINT_PATH]
-        self.params = self.metadata[MODEL_METADATA_KEY_PARAMS]
-        self.input_shapes = self.metadata[MODEL_METADATA_KEY_INPUT_SHAPES]
-
-    def save_metadata(self, path):
-        self.log.debug('dump metadata')
-        dump_json(self.metadata, path)
-
-    def open_session(self):
-        if self.sess is None:
-            self.sess = tf.Session()
+    def _open_saver(self):
+        if self.saver is None:
             self.saver = tf.train.Saver()
-            self.sess.run(tf.global_variables_initializer())
-            # self.summary_writer = tf.summary.FileWriter(self.instance_summary_folder_path, self.sess.graph)
-        else:
-            raise Exception("fail to open tf session")
 
-    def close_session(self):
+    def _close_saver(self):
+        self.saver = None
+
+    def _open_session(self):
+        try:
+            if self.sess is None:
+                self.sess = tf.Session()
+                self.sess.run(tf.global_variables_initializer())
+        except BaseException as e:
+            log_error_trace(self.log.error, e, head='fail to open tf.Session()')
+
+    def _close_session(self):
         if self.sess is not None:
             self.sess.close()
+            self.sess = None
 
-        if self.saver is not None:
-            self.saver = None
-
-        if self.summary_writer is not None:
-            pass
-            # self.summary_writer.close()
-
-    def build(self):
+    def _build(self):
         try:
             with tf.variable_scope(str(self.id)):
                 with tf.variable_scope("misc_ops"):
                     self.log.debug("build_misc_ops")
-                    self.build_misc_ops()
-
-                with tf.variable_scope("hyper_parameter"):
-                    self.log.debug('build_hyper_parameter')
-                    self.hyper_parameter()
-                    self.build_hyper_parameter(self.params)
-
-                self.log.debug('build_input_shapes')
-
-                if self.input_shapes is None:
-                    raise AttributeError("input_shapes not feed")
-                self.build_input_shapes(self.input_shapes)
+                    self._build_misc_ops()
 
                 self.log.debug('build_main_graph')
-                self.build_main_graph()
+                self._build_main_graph()
 
                 with tf.variable_scope('loss_function'):
                     self.log.debug('build_loss_function')
-                    self.build_loss_function()
+                    self._build_loss_function()
 
                 with tf.variable_scope('train_ops'):
                     self.log.debug('build_train_ops')
-                    self.build_train_ops()
+                    self._build_train_ops()
 
-                with tf.variable_scope('summary_ops'):
-                    self.log.debug('build_summary_ops')
-                    self.build_summary_ops()
-
-        except Exception:
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            self.log.error("\n", "".join(traceback.format_tb(exc_traceback)))
+        except BaseException as e:
+            log_error_trace(self.log.error, e)
+            log_error_trace(print, e)
             raise ModelBuildFailError("ModelBuildFailError")
         else:
-            self.is_built = True
+            self._is_built = True
             self.log.info("build success")
 
     def build_input_shapes(self, input_shapes):
@@ -256,18 +243,7 @@ class BaseModel:
         """
         raise NotImplementedError
 
-    def build_hyper_parameter(self, params=None):
-        """load hyper parameter for model
-
-        :param params:
-        :raise NotImplementError
-        if not implemented
-        """
-        if params is not None:
-            for key in self.hyper_param_key:
-                self.__dict__[key] = params[key]
-
-    def build_main_graph(self):
+    def _build_main_graph(self):
         """load main tensor graph
 
         :raise NotImplementError
@@ -275,7 +251,7 @@ class BaseModel:
         """
         raise NotImplementedError
 
-    def build_loss_function(self):
+    def _build_loss_function(self):
         """load loss function of model
 
         :raise NotImplementError
@@ -283,7 +259,7 @@ class BaseModel:
         """
         raise NotImplementedError
 
-    def build_misc_ops(self):
+    def _build_misc_ops(self):
         """load misc operation of model
 
         :raise NotImplementError
@@ -295,7 +271,7 @@ class BaseModel:
         self.global_epoch = tf.get_variable("global_epoch", shape=1, initializer=tf.zeros_initializer)
         self.op_inc_global_step = tf.assign(self.global_epoch, self.global_step + 1, name='op_inc_global_epoch')
 
-    def build_train_ops(self):
+    def _build_train_ops(self):
         """Load train operation of model
 
         :raise NotImplementError
@@ -303,42 +279,27 @@ class BaseModel:
         """
         raise NotImplementedError
 
-    def build_summary_ops(self):
-        """load summary operation for tensorboard
+    def save(self, path=None):
+        if path is None:
+            self.log.info('save directory not specified, use default directory')
+            path = os.path.join(ROOT_PATH, 'instance', self.id)
 
-        :raise NotImplemented
-        if not implemented
-        """
-        pass
+        self.instance_path = path
+        self.save_folder_path = path_join(self.instance_path, 'check_point')
+        self.check_point_path = path_join(self.save_folder_path, 'instance.ckpt')
+        self.metadata_path = path_join(self.instance_path, 'meta.json')
+        setup_directory(self.instance_path)
+        setup_directory(self.save_folder_path)
+        self._save_metadata(self.metadata_path)
 
-    def write_summary(self, sess=None, iter_num=None, dataset=None, summary_writer=None):
-        """write summary of model for tensorboard
+        self.input_shapes_path = path_join(self.instance_path, 'input_shapes.json')
+        self._save_input_shapes(self.input_shapes_path)
+        self.params_path = path_join(self.instance_path, 'params.json')
+        self._save_params(self.params_path)
 
-        :type sess: Session object for tensorflow.Session
-        :type iter_num: int
-        :type dataset: dataset_handler.AbstractDataset
-        :type summary_writer: tensorflow.summary.FileWriter
+        self._open_session()
+        self._open_saver()
 
-        :param sess: session object for tensorflow
-        :param iter_num: current iteration number
-        :param dataset: dataset for train model
-        :param summary_writer: file writer for tensorboard summary
-
-        :raise NotImplementedError
-        if not implemented
-        """
-        pass
-
-    def hyper_parameter(self):
-        self.batch_size = None
-        pass
-
-    def save(self):
-        self.setup_model()
-        self.save_metadata(self.metadata_path)
-
-        if self.sess is None:
-            self.open_session()
         self.saver.save(self.sess, self.check_point_path)
 
         self.log.info("saved at {}".format(self.instance_path))
@@ -346,21 +307,34 @@ class BaseModel:
         return self.instance_path
 
     def load(self, path):
-        path = os.path.join(path, 'instance.meta')
-        self.load_metadata(path)
+        self._load_metadata(os.path.join(path, 'meta.json'))
+        self._load_input_shapes(os.path.join(path, 'input_shapes.json'))
+        self._load_params(os.path.join(path, 'params.json'))
 
-        self.build()
-        self.close_session()
-        self.open_session()
+        self._build()
 
+        self._close_session()
+        self._open_session()
+
+        self._close_saver()
+        self._open_saver()
         self.saver.restore(self.sess, self.check_point_path)
 
     def get_tf_values(self, fetches, feet_dict):
-        self.sess.run(fetches, feet_dict)
+        self.is_built()
 
-    def if_not_ready_to_train(self):
-        if not self.is_built:
-            self.build()
+        return self.sess.run(fetches, feet_dict)
+
+    def is_built(self):
+        if not self._is_built:
+            self._build()
 
         if self.sess is None:
-            self.open_session()
+            self._open_session()
+
+    @staticmethod
+    def to_dummyDataset(**kwargs):
+        dataset = DummyDataset()
+        for key, item in kwargs.items():
+            dataset.add_data(key, item)
+        return dataset
