1f2c65dec3ba074d72f8f6693f63a37d71ed6994
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-28 03:17:49 +0900

    fix test_AAE.py

--- unit_test/model/sklearn_like_model/AE/test_AAE.py
+++ unit_test/model/sklearn_like_model/AE/test_AAE.py
@@ -2,123 +2,117 @@ from script.data_handler.DatasetPackLoader import DatasetPackLoader
 from script.model.sklearn_like_model.AE.AAE import AAE
 
 
-class Test_AAE:
+def test_AAE():
     class_ = AAE
+    data_pack = DatasetPackLoader().load_dataset("MNIST")
+    dataset = data_pack['train']
+    Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
+    sample_X = Xs[:2]
+    sample_Y = Ys[:2]
 
-    def test_mnist(self):
-        class_ = self.class_
-        data_pack = DatasetPackLoader().load_dataset("MNIST")
-        dataset = data_pack['train']
-        Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
-        sample_X = Xs[:2]
-        sample_Y = Ys[:2]
+    model = class_()
+    model.train(Xs, Ys, epoch=1)
 
-        model = class_(dataset.input_shapes)
-        model._build()
+    code = model.code(sample_X)
+    # print("code {code}".format(code=code))
 
-        model.train(Xs, Ys, epoch=1)
+    recon = model.recon(sample_X, sample_Y)
+    # print("recon {recon}".format(recon=recon))
 
-        code = model.code(sample_X)
-        print("code {code}".format(code=code))
+    loss = model.metric(sample_X, sample_Y)
+    # print("loss {}".format(loss))
 
-        recon = model.recon(sample_X, sample_Y)
-        print("recon {recon}".format(recon=recon))
+    # generate(self, zs, Ys)
 
-        loss = model.metric(sample_X, sample_Y)
-        print("loss {}".format(loss))
+    proba = model.predict_proba(sample_X)
+    # print("proba {}".format(proba))
 
-        # generate(self, zs, Ys)
+    predict = model.predict(sample_X)
+    # print("predict {}".format(predict))
 
-        proba = model.proba(sample_X)
-        print("proba {}".format(proba))
+    score = model.score(sample_X, sample_Y)
+    # print("score {}".format(score))
 
-        predict = model.predict(sample_X)
-        print("predict {}".format(predict))
+    path = model.save()
 
-        score = model.score(sample_X, sample_Y)
-        print("score {}".format(score))
+    model = class_()
+    model.load(path)
+    # print('model reloaded')
 
-        path = model.save()
+    code = model.code(sample_X)
+    # print("code {code}".format(code=code))
 
-        model = class_()
-        model.load(path)
-        print('model reloaded')
+    recon = model.recon(sample_X, sample_Y)
+    # print("recon {recon}".format(recon=recon))
 
-        code = model.code(sample_X)
-        print("code {code}".format(code=code))
+    loss = model.metric(sample_X, sample_Y)
+    # print("loss {}".format(loss))
 
-        recon = model.recon(sample_X, sample_Y)
-        print("recon {recon}".format(recon=recon))
+    # generate(self, zs, Ys)
 
-        loss = model.metric(sample_X, sample_Y)
-        print("loss {}".format(loss))
+    proba = model.predict_proba(sample_X)
+    # print("proba {}".format(proba))
 
-        # generate(self, zs, Ys)
+    predict = model.predict(sample_X)
+    # print("predict {}".format(predict))
 
-        proba = model.proba(sample_X)
-        print("proba {}".format(proba))
+    score = model.score(sample_X, sample_Y)
+    # print("score {}".format(score))
 
-        predict = model.predict(sample_X)
-        print("predict {}".format(predict))
 
-        score = model.score(sample_X, sample_Y)
-        print("score {}".format(score))
+def test_AAE_with_noise():
+    class_ = AAE
+    data_pack = DatasetPackLoader().load_dataset("MNIST")
+    dataset = data_pack['train']
+    Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
+    sample_X = Xs[:2]
+    sample_Y = Ys[:2]
 
-    def test_titanic(self):
-        class_ = self.class_
-        data_pack = DatasetPackLoader().load_dataset("titanic")
-        dataset = data_pack['train']
-        Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
-        sample_X = Xs[:2]
-        sample_Y = Ys[:2]
+    model = class_(with_noise=True)
+    model.train(Xs, Ys, epoch=1)
 
-        model = class_(dataset.input_shapes)
-        model._build()
+    code = model.code(sample_X)
+    # print("code {code}".format(code=code))
 
-        model.train(Xs, Ys, epoch=1)
+    recon = model.recon(sample_X, sample_Y)
+    # print("recon {recon}".format(recon=recon))
 
-        code = model.code(sample_X)
-        print("code {code}".format(code=code))
+    loss = model.metric(sample_X, sample_Y)
+    # print("loss {}".format(loss))
 
-        recon = model.recon(sample_X, sample_Y)
-        print("recon {recon}".format(recon=recon))
+    # generate(self, zs, Ys)
 
-        loss = model.metric(sample_X, sample_Y)
-        print("loss {:}".format(loss))
+    proba = model.predict_proba(sample_X)
+    # print("proba {}".format(proba))
 
-        # generate(self, zs, Ys)
+    predict = model.predict(sample_X)
+    # print("predict {}".format(predict))
 
-        proba = model.proba(sample_X)
-        print("proba {}".format(proba))
+    score = model.score(sample_X, sample_Y)
+    # print("score {}".format(score))
 
-        predict = model.predict(sample_X)
-        print("predict {}".format(predict))
+    path = model.save()
 
-        score = model.score(sample_X, sample_Y)
-        print("score {}".format(score))
+    model = class_()
+    model.load(path)
+    # print('model reloaded')
 
-        path = model.save()
+    code = model.code(sample_X)
+    # print("code {code}".format(code=code))
 
-        model = class_()
-        model.load(path)
-        print('model reloaded')
+    recon = model.recon(sample_X, sample_Y)
+    # print("recon {recon}".format(recon=recon))
 
-        code = model.code(sample_X)
-        print("code {code}".format(code=code))
+    loss = model.metric(sample_X, sample_Y)
+    # print("loss {}".format(loss))
 
-        recon = model.recon(sample_X, sample_Y)
-        print("recon {recon}".format(recon=recon))
+    # generate(self, zs, Ys)
 
-        loss = model.metric(sample_X, sample_Y)
-        print("loss {:}".format(loss))
+    proba = model.predict_proba(sample_X)
+    # print("proba {}".format(proba))
 
-        # generate(self, zs, Ys)
+    predict = model.predict(sample_X)
+    # print("predict {}".format(predict))
 
-        proba = model.proba(sample_X)
-        print("proba {}".format(proba))
-
-        predict = model.predict(sample_X)
-        print("predict {}".format(predict))
-
-        score = model.score(sample_X, sample_Y)
-        print("score {}".format(score))
+    score = model.score(sample_X, sample_Y)
+    # print("score {}".format(score))
