bc33e1e283ff6de5cb4d63961ced8b2f5818e2f4
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-28 06:51:04 +0900

    fix test

--- unit_test/model/sklearn_like_model/AE/test_CVAE.py
+++ unit_test/model/sklearn_like_model/AE/test_CVAE.py
@@ -3,9 +3,7 @@ from script.data_handler.DatasetPackLoader import DatasetPackLoader
 from script.model.sklearn_like_model.AE.CVAE import CVAE
 
 
-class_ = CVAE
-
-def test_mnist():
+def test_CVAE():
     class_ = CVAE
     data_pack = DatasetPackLoader().load_dataset("MNIST")
     dataset = data_pack['train']
@@ -13,33 +11,69 @@ def test_mnist():
     sample_X = Xs[:2]
     sample_Y = Ys[:2]
 
-    model = class_(dataset.input_shapes)
-    model._build()
+    model = class_()
     model.train(Xs, Ys, epoch=1)
 
     code = model.code(sample_X, sample_Y)
-    print("code {code}".format(code=code))
+    # print("code {code}".format(code=code))
 
     recon = model.recon(sample_X, sample_Y)
-    print("recon {recon}".format(recon=recon))
+    # print("recon {recon}".format(recon=recon))
 
     loss = model.metric(sample_X, sample_Y)
     loss = np.mean(loss)
-    print("loss {:.4}".format(loss))
+    # print("loss {:.4}".format(loss))
 
     path = model.save()
 
     model = class_()
     model.load(path)
-    print('model reloaded')
+    # print('model reloaded')
 
     code = model.code(sample_X, sample_Y)
-    print("code {code}".format(code=code))
+    # print("code {code}".format(code=code))
 
     recon = model.recon(sample_X, sample_Y)
-    print("recon {recon}".format(recon=recon))
+    # print("recon {recon}".format(recon=recon))
 
     loss = model.metric(sample_X, sample_Y)
     loss = np.mean(loss)
-    print("loss {:.4}".format(loss))
+    # print("loss {:.4}".format(loss))
 
+
+def test_CVAE_with_noise():
+    class_ = CVAE
+    data_pack = DatasetPackLoader().load_dataset("MNIST")
+    dataset = data_pack['train']
+    Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
+    sample_X = Xs[:2]
+    sample_Y = Ys[:2]
+
+    model = class_(with_noise=True)
+    model.train(Xs, Ys, epoch=1)
+
+    code = model.code(sample_X, sample_Y)
+    # print("code {code}".format(code=code))
+
+    recon = model.recon(sample_X, sample_Y)
+    # print("recon {recon}".format(recon=recon))
+
+    loss = model.metric(sample_X, sample_Y)
+    loss = np.mean(loss)
+    # print("loss {:.4}".format(loss))
+
+    path = model.save()
+
+    model = class_()
+    model.load(path)
+    # print('model reloaded')
+
+    code = model.code(sample_X, sample_Y)
+    # print("code {code}".format(code=code))
+
+    recon = model.recon(sample_X, sample_Y)
+    # print("recon {recon}".format(recon=recon))
+
+    loss = model.metric(sample_X, sample_Y)
+    loss = np.mean(loss)
+    # print("loss {:.4}".format(loss))
