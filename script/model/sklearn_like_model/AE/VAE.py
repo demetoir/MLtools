e9140a91d4d7ff0b38e6db652fa801f6f6bdae9c
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-27 18:54:21 +0900

    remake VAE.py with denoise

--- script/model/sklearn_like_model/AE/VAE.py
+++ script/model/sklearn_like_model/AE/VAE.py
@@ -1,25 +1,31 @@
-from script.model.sklearn_like_model.AE.AE import basicAE_DecoderMixIn
-from script.model.sklearn_like_model.AE.BaseAutoEncoder import BaseAutoEncoder
+from script.model.sklearn_like_model.AE.AE import AE, basicAE_Decoder
 from script.util.Stacker import Stacker
 from script.util.tensor_ops import *
 from script.util.summary_func import *
 
 
-class basicVAE_EncoderMixIn:
-    @staticmethod
-    def encoder(Xs, net_shapes, latent_code_size, reuse=False, name='encoder'):
-        with tf.variable_scope(name, reuse=reuse):
-            stack = Stacker(Xs)
-            stack.flatten()
-            for shape in net_shapes:
-                stack.linear_block(shape, relu)
+def basicVAE_Encoder(Xs, net_shapes, latent_code_size, reuse=False, name='encoder'):
+    with tf.variable_scope(name, reuse=reuse):
+        stack = Stacker(Xs)
+        stack.flatten()
+        for shape in net_shapes:
+            stack.linear_block(shape, relu)
 
-            stack.linear_block(latent_code_size * 2, relu)
+        stack.linear_block(latent_code_size * 2, relu)
 
-        return stack.last_layer
+    return stack.last_layer
 
 
-class VAE(BaseAutoEncoder, basicAE_DecoderMixIn, basicVAE_EncoderMixIn):
+class VAE(AE):
+    _input_shape_keys = [
+        'X_shape',
+        'Xs_shape',
+        'X_flatten_size',
+        'z_shape',
+        'zs_shape',
+        'noise_shape'
+
+    ]
     _params_keys = [
         'batch_size',
         'learning_rate',
@@ -30,33 +36,15 @@ class VAE(BaseAutoEncoder, basicAE_DecoderMixIn, basicVAE_EncoderMixIn):
         'z_size',
         'encoder_net_shapes',
         'decoder_net_shapes',
-    ]
-    _input_shape_keys = [
-        'X_shape',
-        'Xs_shape',
-        'X_flatten_size',
-        'z_shape',
-        'zs_shape'
+        'with_noise',
+        'noise_intensity',
     ]
 
-    def __init__(self, verbose=10, **kwargs):
-        super(VAE, self).__init__(verbose, **kwargs)
-
-        self.batch_size = 100
-        self.learning_rate = 0.01
-        self.beta1 = 0.5
-        self.L1_norm_lambda = 0.001
-        self.K_average_top_k_loss = 10
-        self.latent_code_size = 32
-        self.z_size = 32
-        self.encoder_net_shapes = [512, 256, 128]
-        self.decoder_net_shapes = [128, 256, 512]
-
-        self.X_shape = None
-        self.Xs_shape = None
-        self.X_flatten_size = None
-        self.z_shape = None
-        self.zs_shape = None
+    def __init__(self, batch_size=100, learning_rate=0.01, beta1=0.5, L1_norm_lambda=0.001, latent_code_size=32,
+                 z_size=32, encoder_net_shapes=None, decoder_net_shapes=None, with_noise=False, noise_intensity=1.,
+                 verbose=10):
+        super().__init__(batch_size, learning_rate, beta1, L1_norm_lambda, latent_code_size, z_size, encoder_net_shapes,
+                         decoder_net_shapes, with_noise, noise_intensity, verbose)
 
     def build_input_shapes(self, shapes):
         X_shape = shapes['Xs']
@@ -66,28 +54,38 @@ class VAE(BaseAutoEncoder, basicAE_DecoderMixIn, basicVAE_EncoderMixIn):
         z_shape = [self.z_size]
         zs_shape = [None, self.z_size]
 
+        noise_shape = [None] + list(X_shape)
+
         ret = {
             'X_shape': X_shape,
             'Xs_shape': Xs_shape,
             'X_flatten_size': X_flatten_size,
             'z_shape': z_shape,
-            'zs_shape': zs_shape
+            'zs_shape': zs_shape,
+            'noise_shape': noise_shape
         }
         return ret
 
     def _build_main_graph(self):
         self.Xs = placeholder(tf.float32, self.Xs_shape, name='Xs')
         self.zs = placeholder(tf.float32, self.zs_shape, name='zs')
+        self.noise = placeholder(tf.float32, self.noise_shape, name='noise')
 
-        self.h = self.encoder(self.Xs, self.encoder_net_shapes, self.latent_code_size)
+        self.Xs_noised = tf.add(self.Xs, self.noise, name='Xs_noised')
+        if self.with_noise:
+            Xs = self.Xs_noised
+        else:
+            Xs = self.Xs
+
+        self.h = basicVAE_Encoder(Xs, self.encoder_net_shapes, self.latent_code_size)
         self.h = tf.identity(self.h, 'h')
 
         self.mean = tf.identity(self.h[:, :self.z_size], 'mean')
         self.std = tf.identity(tf.nn.softplus(self.h[:, self.z_size:]), 'std')
         self.latent_code = self.mean + self.std * tf.random_normal(tf.shape(self.mean), 0, 1, dtype=tf.float32)
 
-        self.Xs_recon = self.decoder(self.latent_code, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape)
-        self.Xs_gen = self.decoder(self.zs, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape, reuse=True)
+        self.Xs_recon = basicAE_Decoder(self.latent_code, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape)
+        self.Xs_gen = basicAE_Decoder(self.zs, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape, reuse=True)
 
         head = get_scope()
         self.vars = collect_vars(join_scope(head, 'encoder'))
@@ -124,6 +122,3 @@ class VAE(BaseAutoEncoder, basicAE_DecoderMixIn, basicVAE_EncoderMixIn):
     def _build_train_ops(self):
         self.train_op = tf.train.AdamOptimizer(self.learning_rate, self.beta1).minimize(loss=self.loss,
                                                                                         var_list=self.vars)
-
-    def random_z(self):
-        pass
