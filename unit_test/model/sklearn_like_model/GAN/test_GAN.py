122991da30977afb18ea564ea960f68f8b1564ca
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-06-28 02:37:42 +0900

    fix test

--- unit_test/model/sklearn_like_model/GAN/test_GAN.py
+++ unit_test/model/sklearn_like_model/GAN/test_GAN.py
@@ -2,36 +2,33 @@ from script.data_handler.DatasetPackLoader import DatasetPackLoader
 from script.model.sklearn_like_model.GAN.GAN import GAN
 
 
-class Test_GAN:
+def test_GAN():
     class_ = GAN
+    data_pack = DatasetPackLoader().load_dataset("titanic")
+    dataset = data_pack['train']
+    Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
+    sample_X = Xs[:2]
+    # sample_Y = Ys[:2]
 
-    def test_GAN(self):
-        class_ = self.class_
-        data_pack = DatasetPackLoader().load_dataset("titanic")
-        dataset = data_pack['train']
-        Xs, Ys = dataset.full_batch(['Xs', 'Ys'])
-        sample_X = Xs[:2]
-        # sample_Y = Ys[:2]
+    model = class_()
+    model.train(Xs, epoch=1)
 
-        model = class_()
+    metric = model.metric(sample_X)
+    # print(metric)
+
+    gen = model.generate(size=2)
+    # print(gen)
+
+    path = model.save()
+
+    model = class_()
+    model.load(path)
+    # print('model reloaded')
+
+    for i in range(1):
         model.train(Xs, epoch=1)
 
-        metric = model.metric(sample_X)
-        # print(metric)
+    metric = model.metric(sample_X)
+    # print(metric)
 
-        gen = model.generate(size=2)
-        # print(gen)
-
-        path = model.save()
-
-        model = class_()
-        model.load(path)
-        # print('model reloaded')
-
-        for i in range(1):
-            model.train(Xs, epoch=1)
-
-        metric = model.metric(sample_X)
-        # print(metric)
-
-        model.save()
+    model.save()
