818c2aa265b0b4eea7e814004394f3e92db879d5
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-01 22:06:41 +0900

    update bench_code.py

--- script/workbench/bench_code.py
+++ script/workbench/bench_code.py
@@ -2,6 +2,10 @@
 # from script.model.sklearn_like_model.AE.CVAE import CVAE
 import os
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
 from script.model.sklearn_like_model.GAN.C_GAN import C_GAN
 from script.model.sklearn_like_model.AE.AAE import AAE
 from script.model.sklearn_like_model.AE.CVAE import CVAE
@@ -18,7 +22,7 @@ import numpy as np
 ########################################################################################################################
 # print(built-in function) is not good for logging
 from unit_test.model.sklearn_like_model.AE.test_VAE import test_VAE
-from script.util.misc_util import path_join
+from script.util.misc_util import path_join, time_stamp, setup_file
 
 bprint = print
 logger = Logger('bench_code', level='INFO', )
@@ -151,12 +155,6 @@ def test_C_GAN():
     pprint(gen)
 
 
-def plot_1d(x):
-    x = x.reshape([-1])
-    plt.bar([i for i in range(len(x))], x)
-    plt.show()
-
-
 def exp_C_GAN_with_titanic():
     def show(data):
         pass
@@ -197,8 +195,8 @@ def exp_C_GAN_with_titanic():
     pprint(Xs_gen)
     pprint(Xs_gen.shape)
 
-    plot_1d(train_Xs[:1])
-    plot_1d(Xs_gen)
+    # plot_1d(train_Xs[:1])
+    # plot_1d(Xs_gen)
     # plt.plot(train_Xs[:1])
     # plt.show()
 
@@ -257,8 +255,8 @@ def exp_CVAE_with_titanic():
 
     # path = 'C:\\Users\\demetoir_desktop\\PycharmProjects\\MLtools\\instance\\demetoir_C_GAN_2018-06-29_02-33-02'
     # if not os.path.exists(path):
-    cvae = CVAE(learning_rate=0.1, z_size=32, verbose=20)
-    cvae.train(train_Xs, train_Ys, epoch=1000)
+    cvae = CVAE(learning_rate=0.1, z_size=32, verbose=20, KL_D_rate=.05)
+    cvae.train(train_Xs, train_Ys, epoch=1)
     # path = gan.save()
     # print(path)
 
@@ -270,8 +268,9 @@ def exp_CVAE_with_titanic():
 
     Xs_gen = cvae.recon(train_Xs, train_Ys)
 
-    plot_1d(Xs_gen[:1])
-    plot_1d(train_Xs[:1])
+    # plot_1d(Xs_gen[:1])
+    # plot_1d(train_Xs[:1])
+
     #
     # Xs_gen = np.concatenate([Xs_gen, cvae.recon(train_Xs, train_Ys)], axis=0)
     # Xs_gen = np.concatenate([Xs_gen, cvae.recon(train_Xs, train_Ys)], axis=0)
@@ -331,10 +330,134 @@ def exp_CVAE_with_titanic():
     # datapack.to_kaggle_submit_csv(submit_path, predict)
 
 
+def plt_common(plt, fig, path=None, show=False, title=None, extend='.png'):
+    if title is None:
+        title = time_stamp()
+    plt.title(title)
+
+    if path is None:
+        path = path_join('.', 'matplot', title + extend)
+    setup_file(path)
+    fig.savefig(path)
+
+    if show:
+        plt.show()
+
+
+def plot_line(x, path=None, show=False, title=None, extend='.png'):
+    fig = plt.figure()
+
+    plt.plot(x)
+
+    plt_common(plt, fig, path=path, show=show, title=title, extend=extend)
+
+
+def plot_dot(*args, path=None, show=False, title=None, extend='.png'):
+    sns.set()
+    sns.set_style('whitegrid')
+    sns.set_color_codes()
+
+    current_palette = sns.color_palette()
+    print(current_palette)
+    sns.palplot(current_palette)
+
+    fig = plt.figure()
+
+    color_set = ['b', 'r', 'g', 'c', 'm', 'y', 'k', 'w']
+    marker_set = ['.', ',', 'o', 'v', '<', '>', '1', '2', '3', '4', 's', 'p', '*', 'h', 'H', '+', 'x', 'D', 'd',
+                  '|', '_', ]
+    line_set = ['-', '--', '-.', ':']
+    """
+        ``'-'``          solid line style
+        ``'--'``         dashed line style
+        ``'-.'``         dash-dot line style
+        ``':'``          dotted line style
+    """
+
+    """
+        ``'.'``          point marker
+        ``','``          pixel marker
+        ``'o'``          circle marker
+        ``'v'``          triangle_down marker
+        ``'^'``          triangle_up marker
+        ``'<'``          triangle_left marker
+        ``'>'``          triangle_right marker
+        ``'1'``          tri_down marker
+        ``'2'``          tri_up marker
+        ``'3'``          tri_left marker
+        ``'4'``          tri_right marker
+        ``'s'``          square marker
+        ``'p'``          pentagon marker
+        ``'*'``          star marker
+        ``'h'``          hexagon1 marker
+        ``'H'``          hexagon2 marker
+        ``'+'``          plus marker
+        ``'x'``          x marker
+        ``'D'``          diamond marker
+        ``'d'``          thin_diamond marker
+        ``'|'``          vline marker
+        ``'_'``          hline marker
+    """
+
+    new_xs = []
+    for x in args:
+        new_xs += [x, 'b--']
+    xs = new_xs
+
+    plt.plot(*xs)
+
+    plt_common(plt, fig, path=path, show=show, title=title, extend=extend)
+
+
+def hist(x, path=None, show=False, title=None, extend='.png'):
+    fig = plt.figure()
+
+    plt.hist(x)
+
+    plt_common(plt, fig, path=path, show=show, title=title, extend=extend)
+
+
+def scatter(x, y, path=None, show=False, title=None, extend='.png'):
+    fig = plt.figure(x, y)
+
+    plt.scatter(x)
+
+    plt_common(plt, fig, path=path, show=show, title=title, extend=extend)
+
+
+class plot_tools:
+    pass
+
+
+def test_plot_tools():
+    x = np.array([-i for i in range(32)])
+    y = np.array([i for i in range(32)])
+    z = np.array([i * 2 for i in range(32)])
+    w = np.array([i * 4 for i in range(32)])
+    v = np.array([i * 6 for i in range(32)])
+    k = np.array([i * 8 for i in range(32)])
+
+    # plot_line(x)
+    xs = [np.array([i * k for i in range(32)]) for k in range(-10, 10)]
+
+    plot_dot(*xs)
+    # plot_1d(x)
+    #
+    # hist(x)
+
+    # scatter(x, y)
+
+
 @deco_timeit
 def main():
     # titanic_submit()
     # test_C_GAN()
     # exp_C_GAN_with_titanic()
-    exp_CVAE_with_titanic()
+    # exp_CVAE_with_titanic()
+
+    # s = '<>1234sp*hH+xDd|_'
+    # for c in s:
+    #     bprint(f""" '{c}', """, end=' ')
+    test_plot_tools()
+
     pass
