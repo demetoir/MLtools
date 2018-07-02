cacc33017f218058151d0d129e85efda34ac402c
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-03 03:45:14 +0900

    refactoring

--- unit_test/sklearn_like_toolkit/wrapper/helper.py
+++ unit_test/sklearn_like_toolkit/wrapper/helper.py
@@ -1,3 +1,8 @@
+from pprint import pprint
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+
 def get_reg_data():
     import numpy as np
     from sklearn import datasets
@@ -17,3 +22,26 @@ def get_reg_data():
     y_test = diabetes.target[-20:]
 
     return X_train, y_train, X_test, y_test
+
+
+def wrapper_reg_common(reg, ):
+    train_x, train_y, test_x, test_y = get_reg_data()
+
+    reg.fit(train_x, train_y)
+
+    predict = reg.predict(test_x)
+    pprint(predict)
+
+    score = reg.score(train_x, train_y)
+    pprint(score)
+
+    # The coefficients
+    # print('Coefficients: \n', regr.coef_)
+    # The mean squared error
+
+    mse = mean_squared_error(test_y, predict)
+    pprint("Mean squared error: %.2f" % mse)
+
+    # Explained variance score: 1 is perfect prediction
+    score = r2_score(test_y, predict)
+    pprint('Variance score: %.2f' % score)
--- unit_test/sklearn_like_toolkit/wrapper/test_CatBoost_wrapper.py
+++ unit_test/sklearn_like_toolkit/wrapper/test_CatBoost_wrapper.py
@@ -1,31 +1,7 @@
-from sklearn.metrics import mean_squared_error, r2_score
 from script.sklearn_like_toolkit.warpper.catboost_wrapper import CatBoostReg
-from unit_test.sklearn_like_toolkit.wrapper.helper import get_reg_data
+from unit_test.sklearn_like_toolkit.wrapper.helper import wrapper_reg_common
 
 
 def test_CatoostReg_wrapper():
-    train_x, train_y, test_x, test_y = get_reg_data()
-
-    # Create linear regression object
-    regr = CatBoostReg()
-
-    # Train the model using the training sets
-    regr.fit(train_x, train_y)
-
-    # Make predictions using the testing set
-    diabetes_y_pred = regr.predict(test_x)
-    # pprint(diabetes_y_pred)
-
-    score = regr.score(train_x, train_y)
-    # pprint(score)
-
-    # The coefficients
-    # print('Coefficients: \n', regr.coef_)
-    # The mean squared error
-
-    mse = mean_squared_error(test_y, diabetes_y_pred)
-    # print("Mean squared error: %.2f" % mse)
-
-    # Explained variance score: 1 is perfect prediction
-    score = r2_score(test_y, diabetes_y_pred)
-    # print('Variance score: %.2f' % score)
+    reg = CatBoostReg()
+    wrapper_reg_common(reg)
