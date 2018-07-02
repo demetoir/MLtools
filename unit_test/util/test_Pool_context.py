cd28f39d6fd09e74c192be02db98cc4f227fc7f9
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-03 01:25:43 +0900

    add test_Pool_context.py

---
+++ unit_test/util/test_Pool_context.py
@@ -0,0 +1,15 @@
+import time
+from util.Pool_context import Pool_context
+
+
+def test_func(*args, **kwargs):
+    print(args, kwargs)
+    time.sleep(3)
+
+
+def test_pool_context():
+    args = [1, 2, 3]
+    kwargs = {"a": 1, "b": 2, "c": 3}
+    with Pool_context() as pool:
+        for i in range(10):
+            pool.apply_async(test_func, args=args, kwargs=kwargs)
