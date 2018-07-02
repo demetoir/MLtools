64287c7df99c2cfe9485df51b355c7d1d785c0c1
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-03 01:19:01 +0900

    add Pool_context.py

---
+++ script/util/Pool_context.py
@@ -0,0 +1,68 @@
+import multiprocessing as mp
+import tqdm
+from script.util.IdDict import IdDict
+from multiprocessing_on_dill.pool import Pool as _Pool
+from multiprocessing_on_dill.context import TimeoutError
+from queue import Queue
+
+CPU_COUNT = mp.cpu_count()
+
+
+class Pool_context:
+    def __init__(self, processes=CPU_COUNT) -> None:
+        super().__init__()
+        self.processes = processes
+        self.pool = _Pool(processes=processes)
+        self.childs = IdDict()
+        self.tic = 0.01
+        self._id_count = 0
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("waiting process")
+
+        pbar = tqdm.tqdm(total=len(self.childs))
+
+        q = Queue()
+        for id_, child in self.childs.items():
+            q.put((id_, child))
+
+        while q.qsize() > 0:
+            id_, child = q.get()
+            try:
+                child.get(self.tic)
+                pbar.update(1)
+            except TimeoutError:
+                q.put((id_, child))
+
+        pbar.close()
+
+    def put_child(self, child):
+        return self.childs.put(child)
+
+    def get_child(self, id_):
+        return self.childs[id_]
+
+    def apply(self, func, args=None, kwargs=None):
+        if args is None:
+            args = []
+        if kwargs is None:
+            kwargs = {}
+
+        child = self.pool.apply_async(func=func, args=args, kwds=kwargs)
+        return child.get()
+
+    def apply_async(self, func, args=None, kwargs=None):
+        if args is None:
+            args = []
+        if kwargs is None:
+            kwargs = {}
+        child = self.pool.apply_async(func=func, args=args, kwds=kwargs)
+        id_ = self.childs.put(child)
+        return id_
+
+    def get(self, child_id, timeout=None):
+        child = self.childs[child_id]
+        return child.get(timeout)
