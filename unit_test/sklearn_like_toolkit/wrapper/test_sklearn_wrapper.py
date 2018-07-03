2dba3510baecad955361ddb9df83ca2889e1b1bb
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-03 19:31:28 +0900

    add test

--- unit_test/sklearn_like_toolkit/wrapper/test_sklearn_wrapper.py
+++ unit_test/sklearn_like_toolkit/wrapper/test_sklearn_wrapper.py
@@ -1,7 +1,33 @@
-from script.sklearn_like_toolkit.warpper.sklearn_wrapper import skRidgeClf
+from script.sklearn_like_toolkit.warpper.sklearn_wrapper import skRidgeClf, skRidgeCVClf, skPassiveAggressiveClf, \
+    skRadiusNeighborsClf, skKNeighborsClf, skNearestCentroidClf
 from unit_test.sklearn_like_toolkit.wrapper.helper import wrapper_clf_common
 
 
 def test_skRidgeClf():
     clf = skRidgeClf()
+    wrapper_clf_common(clf)
+
+
+def test_skRidgeCVClf():
+    clf = skRidgeCVClf()
+    wrapper_clf_common(clf)
+
+
+def test_skPassiveAggressiveClf():
+    clf = skPassiveAggressiveClf()
+    wrapper_clf_common(clf)
+
+
+def test_skRadiusNeighborsClf():
+    clf = skRadiusNeighborsClf()
+    wrapper_clf_common(clf)
+
+
+def test_skKNeighborsClf():
+    clf = skKNeighborsClf()
+    wrapper_clf_common(clf)
+
+
+def test_skNearestCentroidClf():
+    clf = skNearestCentroidClf()
     wrapper_clf_common(clf)
