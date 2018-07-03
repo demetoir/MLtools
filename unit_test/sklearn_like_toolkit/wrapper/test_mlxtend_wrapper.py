a66e92467df66cbb5076474d6e90ab04de3427e8
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-04 02:43:05 +0900

    add mlxtend wrapper class

--- unit_test/sklearn_like_toolkit/wrapper/test_mlxtend_wrapper.py
+++ unit_test/sklearn_like_toolkit/wrapper/test_mlxtend_wrapper.py
@@ -1,7 +1,23 @@
-from script.sklearn_like_toolkit.warpper.mlxtend_wrapper import mlxLinearReg
+from script.sklearn_like_toolkit.warpper.mlxtend_wrapper import mlxLinearReg, mlxStackingCVReg, mlxStackingReg
+from script.sklearn_like_toolkit.warpper.sklearn_wrapper import skBayesianRidgeReg, skDecisionTreeReg, skLassoReg, \
+    skMLPReg
 from unit_test.sklearn_like_toolkit.wrapper.helper import wrapper_reg_common
 
 
 def test_mlxLinearReg():
     reg = mlxLinearReg()
+    wrapper_reg_common(reg)
+
+
+def test_mlxStackingCVReg():
+    meta = skBayesianRidgeReg()
+    regs = [skDecisionTreeReg(), skLassoReg(), skMLPReg()]
+    reg = mlxStackingCVReg(regs, meta)
+    wrapper_reg_common(reg)
+
+
+def test_mlxStackingReg():
+    meta = skBayesianRidgeReg()
+    regs = [skDecisionTreeReg(), skLassoReg(), skMLPReg()]
+    reg = mlxStackingReg(regs, meta)
     wrapper_reg_common(reg)
