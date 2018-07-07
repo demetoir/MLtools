c6d90ba903d38abda3a43661a0bca4fbca501358
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-08 00:03:45 +0900

    fix lint

--- unit_test/sklearn_like_toolkit/test_HyperOpt.py
+++ unit_test/sklearn_like_toolkit/test_HyperOpt.py
@@ -7,7 +7,7 @@ from script.util.deco import deco_timeit
 
 
 @deco_timeit
-def test_hyperopt():
+def test_hyperOpt():
     data_pack = DatasetPackLoader().load_dataset('titanic')
 
     def fit_clf(kwargs):
@@ -26,36 +26,20 @@ def test_hyperopt():
 
         return score
 
-    space = hp.choice('classifier_type', [
-        {
-            'alpha': hp.loguniform('alpha', -4, 3),
-            'learning_rate': hp.choice('learning_rate', ['constant', 'invscaling', 'adaptive']),
-            'activation': hp.choice('activation', ['relu']),
-            'max_iter': 1000,
-        },
-        # {
-        #   'kernel': hp.choice('svm_kernel', [
-        #           {'ktype': 'linear'},
-        #           {'ktype': 'RBF', 'width': hp.lognormal('svm_rbf_width', 0, 1)},
-
-        #     'criterion': hp.choice('dtree_criterion', ['gini', 'entropy']),
-        #     'max_depth': hp.choice('dtree_max_depth',
-        #                            [None, hp.qlognormal('dtree_max_depth_int', 3, 1, 1)]),
-        #     'min_samples_split': hp.qlognormal('dtree_min_samples_split', 2, 1, 1),
-        # },
-    ])
+    space = hp.choice('classifier_type', [{
+        'alpha': hp.loguniform('alpha', -4, 3),
+        'learning_rate': hp.choice('learning_rate', ['constant', 'invscaling', 'adaptive']),
+        'activation': hp.choice('activation', ['relu']),
+        'max_iter': 1000,
+    }])
 
     opt = HyperOpt()
     best = opt.fit(fit_clf, data_pack, space, 30)
-
-    # mean, std
-    # score, time
-
+    pprint(best)
     pprint(opt.trials)
     pprint(opt.losses)
     pprint(opt.result)
     pprint(opt.opt_info)
     pprint(opt.best_param)
     pprint(opt.best_loss)
-
-    # pprint(opt.statuses)
+    pprint(opt.statuses)
