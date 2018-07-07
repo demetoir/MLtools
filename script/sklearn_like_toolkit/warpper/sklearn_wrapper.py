46678716ffae3d6975f6a0bab08df0dab50e50b0
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-08 00:44:28 +0900

    refactoring skExtraTreesClf.py, add param

--- script/sklearn_like_toolkit/warpper/sklearn_wrapper.py
+++ script/sklearn_like_toolkit/warpper/sklearn_wrapper.py
@@ -37,7 +37,6 @@ from sklearn.ensemble import GradientBoostingRegressor as _GradientBoostingRegre
 from sklearn.ensemble import BaggingClassifier as _BaggingClassifier
 from sklearn.ensemble import BaggingRegressor as _BaggingRegressor
 from sklearn.ensemble import VotingClassifier as _skVotingClassifier
-from sklearn.ensemble import ExtraTreesClassifier as _skExtraTreesClassifier
 from sklearn.tree import ExtraTreeRegressor as _ExtraTreeRegressor
 from sklearn.tree import DecisionTreeRegressor as _DecisionTreeRegressor
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as _skQDA
@@ -117,46 +116,6 @@ class skQDAClf(BaseWrapperClf, _skQDA, metaclass=meta_BaseWrapperClf):
         'store_covariance': False,
         'store_covariances': None,
         'tol': 0.0001
-    }
-
-
-class skExtraTreesClf(BaseWrapperClf, _skExtraTreesClassifier, metaclass=meta_BaseWrapperClf_with_ABC):
-
-    def __init__(self, n_estimators=10, criterion="gini", max_depth=None, min_samples_split=2, min_samples_leaf=1,
-                 min_weight_fraction_leaf=0., max_features="auto", max_leaf_nodes=None, min_impurity_decrease=0.,
-                 min_impurity_split=None, bootstrap=False, oob_score=False, n_jobs=1, random_state=None, verbose=0,
-                 warm_start=False, class_weight=None):
-        _skExtraTreesClassifier.__init__(
-            self, n_estimators, criterion, max_depth, min_samples_split, min_samples_leaf, min_weight_fraction_leaf,
-            max_features, max_leaf_nodes, min_impurity_decrease, min_impurity_split, bootstrap, oob_score, n_jobs,
-            random_state, verbose, warm_start, class_weight)
-        BaseWrapperClf.__init__(self)
-
-    tuning_grid = {
-        'n_estimators': [16, 32, 64],
-        'max_depth': [i for i in range(1, 10, 2)],
-        'min_samples_leaf': [i for i in range(1, 5)],
-        'min_samples_split': [i for i in range(2, 5)],
-    }
-    only_default_params = {
-        'bootstrap': False,
-        'criterion': 'gini',
-        'max_features': 'auto',
-        'max_leaf_nodes': None,
-        'min_impurity_decrease': 0.0,
-        'min_impurity_split': None,
-    }
-    etc_param = {
-        # class weight options
-        'class_weight': None,
-        'min_weight_fraction_leaf': 0.0,
-
-        # etc
-        'n_jobs': 1,
-        'oob_score': False,
-        'random_state': None,
-        'verbose': 0,
-        'warm_start': False,
     }
 
 
