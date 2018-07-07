4b91398a21d8cf760124c14f4ec70353c6feeab0
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-08 00:29:06 +0900

    add random _param

--- script/sklearn_like_toolkit/warpper/skClf_wrapper/skDecisionTreeClf.py
+++ script/sklearn_like_toolkit/warpper/skClf_wrapper/skDecisionTreeClf.py
@@ -1,7 +1,7 @@
+from hyperopt import hp
 from sklearn.tree import DecisionTreeClassifier as _skDecisionTreeClassifier
-
-from sklearn_like_toolkit.base.BaseWrapperClf import BaseWrapperClf
-from sklearn_like_toolkit.base.MixIn import meta_BaseWrapperClf_with_ABC
+from script.sklearn_like_toolkit.base.BaseWrapperClf import BaseWrapperClf
+from script.sklearn_like_toolkit.base.MixIn import meta_BaseWrapperClf_with_ABC
 
 
 class skDecisionTreeClf(BaseWrapperClf, _skDecisionTreeClassifier, metaclass=meta_BaseWrapperClf_with_ABC):
@@ -14,24 +14,30 @@ class skDecisionTreeClf(BaseWrapperClf, _skDecisionTreeClassifier, metaclass=met
             presort)
         BaseWrapperClf.__init__(self)
 
+    HyperOpt_space = hp.choice('classifier_type', [
+        {
+            'max_depth': 1 + hp.randint('max_depth', 10),
+            'min_samples_leaf': 1 + hp.randint('min_samples_leaf', 10),
+            'min_samples_split': 2 + hp.randint('min_samples_split', 10),
+            'criterion': hp.choice('criterion', ['gini', 'entropy']),
+            'splitter': hp.choice('splitter', ['best', 'random']),
+
+            # 'max_leaf_nodes': None,
+            # 'max_features': None,
+            # 'min_impurity_decrease': 0.0,
+            # 'class_weight': None,
+            # 'min_weight_fraction_leaf': 0.0,
+        },
+
+    ])
 
     tuning_grid = {
         'max_depth': [i for i in range(1, 10)],
         'min_samples_leaf': [i for i in range(1, 5)],
         'min_samples_split': [i for i in range(2, 5)],
     }
-    default_only_params = {
-        'criterion': 'gini',
-        'splitter': 'best',
-        'max_leaf_nodes': None,
-        'max_features': None,
-        'min_impurity_decrease': 0.0,
-        'min_impurity_split': None,
-    }
     etc_param = {
         # class weight options
-        'class_weight': None,
-        'min_weight_fraction_leaf': 0.0,
 
         'presort': False,
         'random_state': None,
