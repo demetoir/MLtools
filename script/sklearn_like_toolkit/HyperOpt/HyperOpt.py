9c42caf97f9d2d7ae3e75c729ada8f5f9c210ade
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-09 04:59:18 +0900

    fix HyperOpt.py, add test

--- script/sklearn_like_toolkit/HyperOpt/HyperOpt.py
+++ script/sklearn_like_toolkit/HyperOpt/HyperOpt.py
@@ -3,7 +3,10 @@ from functools import wraps
 import numpy as np
 from hyperopt import fmin, tpe, STATUS_OK, STATUS_FAIL, Trials
 from hyperopt.mongoexp import MongoTrials
-from tqdm import tqdm
+from joblib import Parallel, delayed
+from tqdm import tqdm, trange
+import multiprocessing as mp
+from script.sklearn_like_toolkit.HyperOpt.FreeTrials import FreeTrials
 from script.util.misc_util import log_error_trace
 
 
@@ -52,12 +55,16 @@ def deco_hyperOpt(func, min_best=True, pbar=None):
     return deco_hyperOpt_wrapper
 
 
+CPU_COUNT = mp.cpu_count() - 1
+
+
 class HyperOpt:
 
-    def __init__(self, min_best=True):
+    def __init__(self, min_best=True, n_job=CPU_COUNT):
         self.min_best = min_best
         self._trials = None
         self._best_param = None
+        self.n_job = n_job
 
     @property
     def outer_trials(self):
@@ -114,7 +121,7 @@ class HyperOpt:
         if isinstance(trials, MongoTrials):
             raise TypeError("MongoTrials not support")
 
-    def fit(self, func, space, n_iter, algo=tpe.suggest, trials=None, min_best=None, pbar=True):
+    def fit_serial(self, func, space, n_iter, algo=tpe.suggest, trials=None, min_best=None, pbar=True):
         if min_best is None:
             min_best = self.min_best
 
@@ -140,3 +147,33 @@ class HyperOpt:
         )
 
         return self._trials
+
+    def fit_parallel(self, func, space, n_iter, algo=tpe.suggest, trials=None, min_best=None, pbar=True):
+        self._check_Trials(trials)
+        min_best = self.min_best if min_best is None else min_best
+        trials = FreeTrials() if trials is None else trials
+        range_ = trange if pbar else range
+
+        # current_done = len(trials)
+        base_trials = trials
+        opt = HyperOpt()
+        ret = Parallel(n_jobs=self.n_job)(
+            delayed(opt.fit_serial)(
+                func, space, 1,
+                algo=algo,
+                trials=base_trials.deepcopy(refresh=False),
+                min_best=min_best,
+                pbar=False
+            ) for _ in range_(n_iter))
+
+        for trials in ret:
+            # partial = trials.partial_deepcopy(current_done, current_done + 1)
+            base_trials = base_trials.concat(trials, refresh=False)
+
+        base_trials.refresh()
+        self._trials = base_trials
+
+        return base_trials
+
+    def fit_parallel_async(self, func, data_pack, space, max_eval, algo=tpe.suggest, trials=None, min_best=None):
+        raise NotImplementedError
--- unit_test/sklearn_like_toolkit/test_HyperOpt.py
+++ unit_test/sklearn_like_toolkit/test_HyperOpt.py
@@ -2,8 +2,29 @@ from pprint import pprint
 from hyperopt import hp
 from script.data_handler.DatasetPackLoader import DatasetPackLoader
 from script.sklearn_like_toolkit.ClassifierPack import ClassifierPack
-from script.sklearn_like_toolkit.HyperOpt import HyperOpt
+from script.sklearn_like_toolkit.HyperOpt.FreeTrials import FreeTrials
+from script.sklearn_like_toolkit.HyperOpt.HyperOpt import HyperOpt
 from script.util.deco import deco_timeit
+
+
+data_pack = DatasetPackLoader().load_dataset('titanic')
+
+
+def fit_clf(params):
+    train_set = data_pack['train']
+    train_set.shuffle()
+    train_set, valid_set = train_set.split()
+    train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
+    valid_Xs, valid_Ys = valid_set.full_batch(['Xs', 'Ys'])
+
+    clf_pack = ClassifierPack()
+    clf = clf_pack['skMLPClf']
+    clf = clf.__class__(**params)
+    clf.fit(train_Xs, train_Ys)
+    score = -clf.score(valid_Xs, valid_Ys)
+
+    return score
+
 
 
 @deco_timeit
@@ -14,6 +35,7 @@ def test_hyperOpt():
         params = kwargs['params']
         data_pack = kwargs['data_pack']
         train_set = data_pack['train']
+        train_set.shuffle()
         train_set, valid_set = train_set.split()
         train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
         valid_Xs, valid_Ys = valid_set.full_batch(['Xs', 'Ys'])
@@ -34,7 +56,7 @@ def test_hyperOpt():
     }])
 
     opt = HyperOpt()
-    best = opt.fit(fit_clf, data_pack, space, 30)
+    best = opt.fit_serial(fit_clf, data_pack, space, 10)
     pprint(best)
     pprint(opt.trials)
     pprint(opt.losses)
@@ -43,3 +65,84 @@ def test_hyperOpt():
     pprint(opt.best_param)
     pprint(opt.best_loss)
     pprint(opt.statuses)
+
+@deco_timeit
+def test_HyperOpt_parallel():
+    clf_pack = ClassifierPack()
+    clf = clf_pack['skMLPClf']
+    space = clf.HyperOpt_space
+
+    opt = HyperOpt()
+    trials1 = opt.fit_serial(fit_clf, space, 1, trials=FreeTrials())
+    # pprint(trials)
+    # pprint(opt.trials)
+    # pprint(opt.losses)
+    # pprint(opt.result)
+    # pprint(opt.opt_info)
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+    pprint(len(trials1))
+    # pprint(len(opt.outer_trials))
+
+    trials2 = opt.fit_serial(fit_clf, space, 2, trials=FreeTrials())
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+    pprint(len(trials2))
+
+    # pprint(trials1.trials)
+    # pprint(trials2.trials)
+    # pprint(trials1.__dict__['_dynamic_trials'])
+    # pprint(trials2.__dict__['_dynamic_trials'])
+
+    trials3 = trials1.concat(trials2)
+    # pprint(trials3.__dict__['_dynamic_trials'])
+
+    pprint(len(trials3))
+
+    trials3 = opt.fit_serial(fit_clf, space, 1, trials=trials3)
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+    # pprint(trials3.__dict__['_dynamic_trials'])
+    pprint(len(trials3))
+
+    trials4 = opt.fit_serial(fit_clf, space, 2, trials=FreeTrials.deepcopy(trials3))
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+    # pprint(trials3.__dict__['_dynamic_trials'])
+    pprint(len(trials4))
+
+    trials5 = opt.fit_serial(fit_clf, space, 3, trials=FreeTrials.deepcopy(trials3))
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+    # pprint(trials3.__dict__['_dynamic_trials'])
+    pprint(len(trials5))
+
+    trials6 = trials4.concat(trials5)
+    pprint(len(trials6))
+
+    trials6 = opt.fit_serial(fit_clf, space, 2, trials=trials6)
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+    # pprint(trials3.__dict__['_dynamic_trials'])
+    pprint(len(trials6))
+
+    partial = trials6[:5]
+    partial = FreeTrials.partial_deepcopy(trials6, 0, 5)
+    pprint(partial)
+    pprint(len(partial))
+
+    partial = opt.fit_serial(fit_clf, space, 4, trials=partial)
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+    # pprint(trials3.__dict__['_dynamic_trials'])
+
+    partial = opt.fit_serial(fit_clf, space, 30, trials=FreeTrials())
+    pprint(opt.best_param)
+    pprint(opt.best_loss)
+
+    n = 100
+    partial = opt.fit_parallel(fit_clf, space, n, trials=FreeTrials())
+    # pprint(opt.best_param)
+    pprint(opt.best_loss)
+    pprint(len(opt.trials))
+    # pprint(opt.trials)
