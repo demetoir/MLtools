c63b34aa557647f266eccaf9404ea0085a5d5cbb
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-11 22:20:50 +0900

    fix HousePrices.py

--- script/data_handler/HousePrices.py
+++ script/data_handler/HousePrices.py
@@ -5,13 +5,48 @@ import numpy as np
 import os
 import sys
 
+from script.data_handler.Base_df_null_handler import df_add_col_num
 from script.util.misc_util import path_join
 
-
-def df_add_col_num(df):
-    ret_df = None
-
-    return ret_df
+df_keys = [
+    '1stFlrSF', '2ndFlrSF', '3SsnPorch', 'Alley', 'BedroomAbvGr',
+    'BldgType', 'BsmtCond', 'BsmtExposure', 'BsmtFinSF1', 'BsmtFinSF2',
+    'BsmtFinType1', 'BsmtFinType2', 'BsmtFullBath', 'BsmtHalfBath',
+    'BsmtQual', 'BsmtUnfSF', 'CentralAir', 'Condition1', 'Condition2',
+    'Electrical', 'EnclosedPorch', 'ExterCond', 'ExterQual', 'Exterior1st',
+    'Exterior2nd', 'Fence', 'FireplaceQu', 'Fireplaces', 'Foundation',
+    'FullBath', 'Functional', 'GarageArea', 'GarageCars', 'GarageCond',
+    'GarageFinish', 'GarageQual', 'GarageType', 'GarageYrBlt', 'GrLivArea',
+    'HalfBath', 'Heating', 'HeatingQC', 'HouseStyle', 'Id', 'KitchenAbvGr',
+    'KitchenQual', 'LandContour', 'LandSlope', 'LotArea', 'LotConfig',
+    'LotFrontage', 'LotShape', 'LowQualFinSF', 'MSSubClass', 'MSZoning',
+    'MasVnrArea', 'MasVnrType', 'MiscFeature', 'MiscVal', 'MoSold',
+    'Neighborhood', 'OpenPorchSF', 'OverallCond', 'OverallQual',
+    'PavedDrive', 'PoolArea', 'PoolQC', 'RoofMatl', 'RoofStyle',
+    'SaleCondition', 'SalePrice', 'SaleType', 'ScreenPorch', 'Street',
+    'TotRmsAbvGrd', 'TotalBsmtSF', 'Utilities', 'WoodDeckSF', 'YearBuilt',
+    'YearRemodAdd', 'YrSold'
+]
+df_Xs_keys = [
+    '1stFlrSF', '2ndFlrSF', '3SsnPorch', 'Alley', 'BedroomAbvGr',
+    'BldgType', 'BsmtCond', 'BsmtExposure', 'BsmtFinSF1', 'BsmtFinSF2',
+    'BsmtFinType1', 'BsmtFinType2', 'BsmtFullBath', 'BsmtHalfBath',
+    'BsmtQual', 'BsmtUnfSF', 'CentralAir', 'Condition1', 'Condition2',
+    'Electrical', 'EnclosedPorch', 'ExterCond', 'ExterQual', 'Exterior1st',
+    'Exterior2nd', 'Fence', 'FireplaceQu', 'Fireplaces', 'Foundation',
+    'FullBath', 'Functional', 'GarageArea', 'GarageCars', 'GarageCond',
+    'GarageFinish', 'GarageQual', 'GarageType', 'GarageYrBlt', 'GrLivArea',
+    'HalfBath', 'Heating', 'HeatingQC', 'HouseStyle', 'Id', 'KitchenAbvGr',
+    'KitchenQual', 'LandContour', 'LandSlope', 'LotArea', 'LotConfig',
+    'LotFrontage', 'LotShape', 'LowQualFinSF', 'MSSubClass', 'MSZoning',
+    'MasVnrArea', 'MasVnrType', 'MiscFeature', 'MiscVal', 'MoSold',
+    'Neighborhood', 'OpenPorchSF', 'OverallCond', 'OverallQual',
+    'PavedDrive', 'PoolArea', 'PoolQC', 'RoofMatl', 'RoofStyle',
+    'SaleCondition', 'SaleType', 'ScreenPorch', 'Street', 'TotRmsAbvGrd',
+    'TotalBsmtSF', 'Utilities', 'WoodDeckSF', 'YearBuilt', 'YearRemodAdd',
+    'YrSold'
+]
+df_Ys_key = 'SalePrice'
 
 
 def load_merge_set(path):
@@ -25,6 +60,8 @@ def load_merge_set(path):
         train = pd.read_csv(train_path)
         test = pd.read_csv(test_path)
         merged = pd.concat([train, test], axis=0)
+        merged = df_add_col_num(merged, zfill_width=2)
+
         merged.to_csv(merged_path, index=False)
     return merged
 
