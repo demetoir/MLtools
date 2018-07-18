b3255e85fec2676dde2a469e06d598342e6fc609
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-19 02:51:22 +0900

    fix test

--- unit_test/data_handler/test_wine_quality.py
+++ unit_test/data_handler/test_wine_quality.py
@@ -1,20 +1,50 @@
-from script.data_handler.HousePrices import HousePricesHelper
-from script.util.deco import deco_timeit
-from script.util.misc_util import path_join
+from script.data_handler.wine_quality import load_merge_data, wine_quality_transformer, wine_qualityPack
 
 
-@deco_timeit
-def test_HousePrices_dataset():
-    dataset_path = """C:\\Users\\demetoir_desktop\\PycharmProjects\\MLtools\\data\\HousePrices"""
+def test_wine_quality_transformer():
+    df_Xs_keys = [
+        'col_0_fixed_acidity', 'col_1_volatile_acidity', 'col_2_citric_acid',
+        'col_3_residual_sugar', 'col_4_chlorides', 'col_5_free_sulfur_dioxide',
+        'col_6_total_sulfur_dioxide', 'col_7_density', 'col_8_pH',
+        'col_9_sulphates', 'col_10_alcohol', 'col_12_color'
+    ]
+    df_Ys_key = 'col_11_quality'
 
-    merge_df = HousePricesHelper.load_merge_set(dataset_path)
+    merge_df = load_merge_data(cache=False)
 
-    merge_null_clean = HousePricesHelper.null_cleaning(merge_df)
+    print(merge_df.keys())
+    transformer = wine_quality_transformer(merge_df, df_Xs_keys, df_Ys_key)
+    transformer.boilerplate_maker('./gen_code.py')
+    merge_df = transformer.transform()
+    print(merge_df.info())
+    transformer.plot_all(merge_df, merge_df.keys(), df_Ys_key)
 
-    merge_type_cast = HousePricesHelper.type_casting(merge_null_clean)
 
-    transformed = HousePricesHelper.transform(merge_type_cast)
+def load_wine_quality_dataset():
+    dataset_path = """C:\\Users\\demetoir_desktop\\PycharmProjects\\MLtools\\data\\winequality"""
+    dataset_pack = wine_qualityPack().load(dataset_path)
+    dataset_pack.shuffle()
+    dataset_pack.split('data', 'train', 'test', rate=(7, 3))
+    train_set = dataset_pack['train']
+    test_set = dataset_pack['test']
 
-    train_df, test_df = HousePricesHelper.train_test_split(transformed)
-    train_df.to_csv(path_join(dataset_path, 'transformed_train.csv'), index=False)
-    test_df.to_csv(path_join(dataset_path, 'transformed_test.csv'), index=False)
+    train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
+    test_Xs, test_Ys = test_set.full_batch(['Xs', 'Ys'])
+    sample_xs = train_Xs[:5]
+    sample_Ys = train_Ys[:5]
+    return train_Xs, train_Ys, test_Xs, test_Ys, sample_xs, sample_Ys
+
+
+def test_wine_quality_dataset_pack():
+    dataset_path = """C:\\Users\\demetoir_desktop\\PycharmProjects\\MLtools\\data\\winequality"""
+    dataset_pack = wine_qualityPack().load(dataset_path)
+
+    dataset = dataset_pack['data']
+    df = dataset.to_DataFrame()
+    print(df.info())
+
+    dataset.shuffle(random_state=7)
+    train_set, test_set = dataset.split((7, 3))
+
+    train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
+    test_Xs, test_Ys = train_set.full_batch(['Xs', 'Ys'])
