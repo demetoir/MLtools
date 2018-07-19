f8cfb8e7261ed12ac38b08566e854db2c23c8798
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-19 18:03:16 +0900

    fix model

--- script/model/sklearn_like_model/AE/VAE.py
+++ script/model/sklearn_like_model/AE/VAE.py
@@ -1,41 +1,165 @@
-from script.model.sklearn_like_model.AE.AE import AE, basicAE_Decoder
+from script.model.sklearn_like_model.AE.AE import AE
 from script.util.Stacker import Stacker
 from script.util.tensor_ops import *
 from script.util.summary_func import *
 
 
-def basicVAE_Encoder(Xs, net_shapes, latent_code_size, reuse=False, name='encoder'):
-    with tf.variable_scope(name, reuse=reuse):
-        stack = Stacker(Xs)
-        stack.flatten()
-        for shape in net_shapes:
-            stack.linear_block(shape, relu)
+def common_linear_stack(stack: Stacker, net_shapes, bn=True, activation='relu') -> Stacker:
+    for shape in net_shapes:
+        stack.linear(shape)
+        if bn:
+            stack.bn()
+        stack.activation(activation)
+        stack.lrelu()
+    return stack
 
-        stack.linear_block(latent_code_size * 2, relu)
+
+def encoder_head(Xs):
+    stack = Stacker(Xs)
+    stack.flatten()
+    return stack
+
+
+def encoder_tail(stack: Stacker, latent_code_size, bn=False, activation='none') -> Stacker:
+    stack.linear(latent_code_size * 2)
+    if bn:
+        stack.bn()
+    stack.activation(activation)
+    return stack
+
+
+def basicVAE_Encoder(Xs, net_shapes, latent_code_size,
+                     linear_stack_bn=False, linear_stack_activation='relu',
+                     tail_bn=False, tail_activation='none',
+                     reuse=False, name='encoder'):
+    with tf.variable_scope(name, reuse=reuse):
+        stack = encoder_head(Xs)
+        stack = common_linear_stack(stack, net_shapes, bn=linear_stack_bn, activation=linear_stack_activation)
+        stack = encoder_tail(stack, latent_code_size, bn=tail_bn, activation=tail_activation)
+    return stack.last_layer
+
+
+def decoder_head(latents) -> Stacker:
+    stack = Stacker(latents)
+    return stack
+
+
+def decoder_tail(stack: Stacker, flatten_size, output_shape, bn=False, activation='sigmoid') -> Stacker:
+    stack.linear(flatten_size)
+    if bn:
+        stack.bn()
+    stack.activation(activation)
+    stack.reshape(output_shape)
+    return stack
+
+
+def basicVAE_Decoder(latents, net_shapes, flatten_size, output_shape,
+                     linear_stack_bn=False, linear_stack_activation='relu',
+                     tail_bn=False, tail_activation='sigmoid',
+
+                     reuse=False, name='decoder'):
+    with tf.variable_scope(name, reuse=reuse):
+        stack = decoder_head(latents)
+        stack = common_linear_stack(stack, net_shapes, bn=linear_stack_bn, activation=linear_stack_activation)
+        stack = decoder_tail(stack, flatten_size, output_shape, bn=tail_bn, activation=tail_activation)
 
     return stack.last_layer
 
 
-class VAE(AE):
-    _params_keys = [
-        'batch_size',
-        'learning_rate',
-        'beta1',
-        'L1_norm_lambda',
-        'K_average_top_k_loss',
-        'code_size',
-        'z_size',
-        'encoder_net_shapes',
-        'decoder_net_shapes',
-        'with_noise',
-        'noise_intensity',
-    ]
+class VAE_loss_builder_MixIn:
+    def __init__(self):
+        cls = self.__class__
+        self._loss_builder_funcs = {
+            'VAE': cls.VAE_loss,
+            'MSE_with_KL': cls.MSE_with_KL_loss,
+            'RMSE_with_KL': cls.RMSE_with_KL_loss,
+            'recon_only': cls.recon_only_loss,
+            'MSE_only': cls.MSE_only_loss,
+            'RMSE_only': cls.RMSE_only_loss,
+        }
 
+    @property
+    def loss_names(self):
+        return self._loss_builder_funcs.keys()
+
+    @staticmethod
+    def _recon_error(X, X_out):
+        cross_entropy = tf.reduce_sum(X * tf.log(X_out) + (1 - X) * tf.log(1 - X_out), axis=1)
+        recon_error = -1 * cross_entropy
+        return recon_error
+
+    @staticmethod
+    def _regularization_error(mean, std, KL_D_rate):
+        KL_Divergence = 0.5 * tf.reduce_sum(
+            1 - tf.log(tf.square(std) + 1e-8) + tf.square(mean) + tf.square(std), axis=1)
+
+        regularization_error = KL_Divergence * KL_D_rate
+        return regularization_error
+
+    @staticmethod
+    def _MSE(X, X_out):
+        MSE = tf.reduce_sum(tf.squared_difference(X, X_out), axis=1)
+        return MSE
+
+    @staticmethod
+    def _RMSE(X, X_out):
+        RMSE = tf.sqrt(tf.reduce_sum(tf.squared_difference(X, X_out), axis=1))
+        return RMSE
+
+    def VAE_loss(self, X, X_out, mean, std, KL_D_rate):
+        # in autoencoder's perspective loss can be divide to reconstruct error and regularization error
+        recon_error = self._recon_error(X, X_out)
+        regularization_error = self._regularization_error(mean, std, KL_D_rate)
+
+        loss = recon_error + regularization_error
+        return loss
+
+    def MSE_with_KL_loss(self, X, X_out, mean, std, KL_D_rate):
+        regularization_error = self._regularization_error(mean, std, KL_D_rate)
+        MSE = self._MSE(X, X_out)
+
+        loss = MSE + regularization_error
+        return loss
+
+    def RMSE_with_KL_loss(self, X, X_out, mean, std, KL_D_rate):
+        RMSE = self._RMSE(X, X_out)
+        regularization_error = self._regularization_error(mean, std, KL_D_rate)
+
+        loss = RMSE + regularization_error
+        return loss
+
+    def RMSE_only_loss(self, X, X_out, mean, std, KL_D_rate):
+        RMSE = self._RMSE(X, X_out)
+
+        return RMSE
+
+    def recon_only_loss(self, X, X_out, mean, std, KL_D_rate):
+        recon_error = self._recon_error(X, X_out)
+
+        return recon_error
+
+    def MSE_only_loss(self, X, X_out, mean, std, KL_D_rate):
+        MSE = self._MSE(X, X_out)
+
+        return MSE
+
+    def _build_VAE_loss(self, X, X_out, mean, std, KL_D_rate, loss_type='VAE'):
+        return self._loss_builder_funcs[loss_type](self, X, X_out, mean, std, KL_D_rate)
+
+
+class VAE(AE, VAE_loss_builder_MixIn):
     def __init__(self, batch_size=100, learning_rate=0.01, beta1=0.5, L1_norm_lambda=0.001, latent_code_size=32,
                  encoder_net_shapes=(512,), decoder_net_shapes=(512,), with_noise=False, noise_intensity=1.,
-                 verbose=10):
-        super(VAE, self).__init__(batch_size, learning_rate, beta1, L1_norm_lambda, latent_code_size,
-                                  encoder_net_shapes, decoder_net_shapes, with_noise, noise_intensity, verbose)
+                 loss_type='VAE', encoder_kwargs=None, decoder_kwargs=None,
+                 KL_D_rate=0.01, verbose=10):
+        AE.__init__(
+            self, batch_size, learning_rate, beta1, L1_norm_lambda, latent_code_size,
+            encoder_net_shapes, decoder_net_shapes, with_noise, noise_intensity,
+            encoder_kwargs, decoder_kwargs, verbose)
+        VAE_loss_builder_MixIn.__init__(self)
+
+        self.KL_D_rate = KL_D_rate
+        self.loss_type = loss_type
 
     def _build_main_graph(self):
         self.Xs = placeholder(tf.float32, self.Xs_shape, name='Xs')
@@ -48,15 +172,24 @@ class VAE(AE):
         else:
             Xs = self.Xs
 
-        self.h = basicVAE_Encoder(Xs, self.encoder_net_shapes, self.latent_code_size)
+        self.h = basicVAE_Encoder(Xs, self.encoder_net_shapes, self.latent_code_size, **self.encoder_kwargs)
         self.h = tf.identity(self.h, 'h')
 
-        self.mean = tf.identity(self.h[:, :self.z_size], 'mean')
-        self.std = tf.identity(tf.nn.softplus(self.h[:, self.z_size:]), 'std')
+        self.mean = tf.identity(self.h[:, :self.latent_code_size], 'mean')
+        self.std = tf.identity(tf.nn.softplus(self.h[:, self.latent_code_size:]), 'std')
+        # self.mean = tf.nn.tanh(self.mean)
+        self.std = tf.nn.leaky_relu(self.std)
+
         self.latent_code = self.mean + self.std * tf.random_normal(tf.shape(self.mean), 0, 1, dtype=tf.float32)
 
-        self.Xs_recon = basicAE_Decoder(self.latent_code, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape)
-        self.Xs_gen = basicAE_Decoder(self.zs, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape, reuse=True)
+        self.Xs_recon = basicVAE_Decoder(
+            self.latent_code, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape,
+            **self.decoder_kwargs
+        )
+        self.Xs_gen = basicVAE_Decoder(
+            self.zs, self.decoder_net_shapes, self.X_flatten_size, self.Xs_shape, reuse=True,
+            **self.decoder_kwargs
+        )
 
         head = get_scope()
         self.vars = collect_vars(join_scope(head, 'encoder'))
@@ -68,26 +201,7 @@ class VAE(AE):
         mean = self.mean
         std = self.std
 
-        self.cross_entropy = tf.reduce_sum(X * tf.log(X_out) + (1 - X) * tf.log(1 - X_out), axis=1)
-        self.KL_Divergence = 0.5 * tf.reduce_sum(
-            1 - tf.log(tf.square(std) + 1e-8) + tf.square(mean) + tf.square(std), axis=1)
-
-        # in autoencoder's perspective loss can be divide to reconstruct error and regularization error
-        # self.recon_error = -1 * self.cross_entropy
-        # self.regularization_error = self.KL_Divergence
-        # self.loss = self.recon_error + self.regularization_error
-
-        # only cross entropy loss also work
-        # self.loss = -1 * self.cross_entropy
-
-        # using MSE than cross entropy loss also work but slow
-        # self.MSE= tf.reduce_sum(tf.squared_difference(X, X_out), axis=1)
-        # self.loss = self.MSE + self.KL_Divergence
-
-        # this one also work
-        # self.loss = self.MSE
-
-        self.loss = tf.add(-1 * self.cross_entropy, self.KL_Divergence, name='loss')
+        self.loss = self._build_VAE_loss(X, X_out, std, mean, self.KL_D_rate, self.loss_type)
         self.loss_mean = tf.reduce_mean(self.loss, name='loss_mean')
 
     def _build_train_ops(self):
