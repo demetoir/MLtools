428ca01207eb4f30d7e0582df328e26848942c7f
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-07-24 18:37:27 +0900

    fix, add DF_PlotTools

--- script/data_handler/Base/df_plotterMixIn.py
+++ script/data_handler/Base/df_plotterMixIn.py
@@ -1,6 +1,9 @@
+from script.util.MixIn import LoggerMixIn
 from script.util.PlotTools import PlotTools
 from script.util.JobPool import JobPool
 import pandas as pd
+
+from script.util.misc_util import log_error_trace
 
 DF = pd.DataFrame
 Series = pd.Series
@@ -15,7 +18,7 @@ def deco_exception_catch(func):
             return func(*args, **kwargs)
         except BaseException as e:
             self.log.warn(f'\nfail {func.__name__}, {key}\n')
-            print(e)
+            log_error_trace(self.log.warn, e)
 
     return wrapper
 
@@ -69,3 +72,101 @@ class df_plotterMixIn:
                 pool.apply_async(self._plot_count, args=args)
                 pool.apply_async(self._plot_violin, args=args)
                 pool.apply_async(self._plot_joint2d, args=args)
+
+
+class DF_PlotTools(LoggerMixIn):
+    def __init__(self, df, y_key, n_job=7, path=None):
+        super().__init__()
+        self.plot = PlotTools()
+        self.n_job = n_job
+        self.df = df
+        self.y_key = y_key
+
+        self.path = path
+        if self.path is None:
+            self.path = "./plot_outs"
+
+    def plot_all(self, ):
+        with JobPool(self.n_job) as pool:
+            keys = self.df.keys()
+            df = self.df
+
+            # plot dist
+            for key in keys:
+                args = [df, key]
+                pool.apply_async(self.plot_dist, args)
+
+            # plot count
+            for key in keys:
+                args = [df, key]
+                pool.apply_async(self.plot_countbar, args)
+
+            # plot violin
+            for key in keys:
+                args = [df, key, self.y_key]
+                pool.apply_async(self.plot_violin, args)
+
+                args = [df, self.y_key, key]
+                pool.apply_async(self.plot_violin, args)
+
+            # plot_joint2d
+            for key in keys:
+                args = [df, key, self.y_key]
+                pool.apply_async(self.plot_joint2d, args)
+
+                args = [df, self.y_key, key]
+                pool.apply_async(self.plot_joint2d, args)
+
+            # plot countbar_groupby
+            for a_key in keys:
+                args = [df, a_key, self.y_key]
+                pool.apply_async(self.plot_count_groupby, args)
+
+    @deco_exception_catch
+    def plot_dist(self, df: DF, col_key: str, title=None):
+        if title is None:
+            title = f'{col_key}_plot_dist'
+
+        self.plot.dist(df, col_key, title=title, path=f"./matplot/{title}.png")
+
+    @deco_exception_catch
+    def plot_countbar(self, df: DF, col_key: str, title=None):
+        if title is None:
+            title = f'{col_key}_plot_count_bar'
+
+        self.plot.count(df, col_key, title=title, path=f"./matplot/{title}.png")
+
+    @deco_exception_catch
+    def plot_violin(self, df: DF, a_col, b_col, title=None):
+        if title is None:
+            title = f'{a_col}_{b_col}_plot_violin'
+
+        self.plot.violin_plot(df, a_col, b_col, path=f"./matplot/{title}.png", title=title)
+
+    @deco_exception_catch
+    def plot_joint2d(self, df: DF, a_col, b_col, title=None):
+        if title is None:
+            title = f'{a_col}_{b_col}_plot_joint2d'
+
+        self.plot.joint_2d(df, a_col, b_col, path=f"./matplot/{title}.png", title=title)
+
+    @deco_exception_catch
+    def plot_dist_groupby(self, df: DF, a_col, groupby_cols, title=None):
+        if title is None:
+            title = f'{a_col}_groupby_{groupby_cols}_plot_dist_groupby'
+
+        self.plot.dist_groupby(df, a_col, groupby_cols, title=title, path=f"./matplot/{title}.png")
+
+    @deco_exception_catch
+    def plot_count_groupby(self, df: DF, a_col, groupby_cols, title=None):
+        if title is None:
+            title = f'{a_col}_groupby_{groupby_cols}_count'
+
+        self.plot.count(df, a_col, groupby_cols, title=title, path=f"./matplot/{title}.png")
+
+    @deco_exception_catch
+    def plot_violin_groupby(self, df: DF, a_col, b_col, groupby_cols, title=None):
+        if title is None:
+            title = f'{a_col}_{b_col}_groupby_{groupby_cols}_violin'
+
+        self.plot.violin_plot(df, a_col, b_col, groupby_cols, title=title, path=f"./matplot/{title}.png")
