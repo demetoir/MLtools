d69324c4d72837af186f3e2b07ec9966bc72725e
demetoir <wnsqlehlswk@naver.com>
2018-08-09 00:46:15 +0900

    fix kwargs

--- script/sklearn_like_toolkit/warpper/catboost_wrapper.py
+++ script/sklearn_like_toolkit/warpper/catboost_wrapper.py
@@ -33,42 +33,13 @@ class CatBoostClf(BaseWrapperClf, _CatBoostClassifier, metaclass=meta_BaseWrappe
         'od_wait': None
     }
 
-    def __init__(
-            self, iterations=None, learning_rate=None, depth=None, l2_leaf_reg=None, model_size_reg=None, rsm=None,
-            loss_function='Logloss', border_count=None, feature_border_type=None, fold_permutation_block_size=None,
-            od_pval=None, od_wait=None, od_type=None, nan_mode=None, counter_calc_method=None,
-            leaf_estimation_iterations=None, leaf_estimation_method=None, thread_count=None, random_seed=None,
-            use_best_model=None, verbose=None, silent=None, logging_level=None, metric_period=None,
-            ctr_leaf_count_limit=None, store_all_simple_ctr=None, max_ctr_complexity=None, has_time=None,
-            allow_const_label=None, classes_count=None, class_weights=None, one_hot_max_size=None,
-            random_strength=None, name=None, ignored_features=None, train_dir=None, custom_loss=None,
-            custom_metric=None, eval_metric=None, bagging_temperature=None, save_snapshot=None, snapshot_file=None,
-            fold_len_multiplier=None, used_ram_limit=None, gpu_ram_part=None, pinned_memory_size=None,
-            allow_writing_files=None, final_ctr_computation_mode=None, approx_on_full_history=None,
-            boosting_type=None, simple_ctr=None, combinations_ctr=None, per_feature_ctr=None, ctr_description=None,
-            task_type=None, device_config=None, devices=None, bootstrap_type=None, subsample=None, max_depth=None,
-            n_estimators=None, num_boost_round=None, num_trees=None, colsample_bylevel=None, random_state=None,
-            reg_lambda=None, objective=None, eta=None, max_bin=None, scale_pos_weight=None,
-            gpu_cat_features_storage=None, data_partition=None, metadata=None):
+    def __init__(self, **kwargs):
         # logging_level = 'Silent'
+        # silent = True
+        # kwargs['silent'] = True
         warnings.filterwarnings(module='sklearn*', action='ignore', category=DeprecationWarning)
-        silent = True
         BaseWrapperClf.__init__(self)
-
-        _CatBoostClassifier.__init__(
-            self, iterations, learning_rate, depth, l2_leaf_reg, model_size_reg, rsm, loss_function,
-            border_count, feature_border_type, fold_permutation_block_size, od_pval, od_wait, od_type,
-            nan_mode, counter_calc_method, leaf_estimation_iterations, leaf_estimation_method,
-            thread_count, random_seed, use_best_model, verbose, silent, logging_level, metric_period,
-            ctr_leaf_count_limit, store_all_simple_ctr, max_ctr_complexity, has_time, allow_const_label,
-            classes_count, class_weights, one_hot_max_size, random_strength, name, ignored_features,
-            train_dir, custom_loss, custom_metric, eval_metric, bagging_temperature, save_snapshot,
-            snapshot_file, fold_len_multiplier, used_ram_limit, gpu_ram_part, pinned_memory_size,
-            allow_writing_files, final_ctr_computation_mode, approx_on_full_history, boosting_type,
-            simple_ctr, combinations_ctr, per_feature_ctr, ctr_description, task_type, device_config,
-            devices, bootstrap_type, subsample, max_depth, n_estimators, num_boost_round, num_trees,
-            colsample_bylevel, random_state, reg_lambda, objective, eta, max_bin, scale_pos_weight,
-            gpu_cat_features_storage, data_partition, metadata)
+        _CatBoostClassifier.__init__(self, **kwargs)
 
     @property
     def feature_importances(self):
@@ -84,7 +55,6 @@ class CatBoostReg(_CatBoostRegressor, BaseWrapperReg, metaclass=meta_BaseWrapper
         'learning_rate': hp.loguniform('learning_rate', -6, 0),
         'l2_leaf_reg': hp.uniform('l2_leaf_reg', 0, 1),
     }
-
     tuning_grid = {
         # 'iterations': [2, 4, 8, ],
         # 'depth': [i for i in range(4, 10 + 1, 2)],
@@ -102,37 +72,9 @@ class CatBoostReg(_CatBoostRegressor, BaseWrapperReg, metaclass=meta_BaseWrapper
         'od_wait': None
     }
 
-    def __init__(
-            self, iterations=None, learning_rate=None, depth=None, l2_leaf_reg=None, model_size_reg=None, rsm=None,
-            loss_function='RMSE', border_count=None, feature_border_type=None, fold_permutation_block_size=None,
-            od_pval=None, od_wait=None, od_type=None, nan_mode=None, counter_calc_method=None,
-            leaf_estimation_iterations=None, leaf_estimation_method=None, thread_count=None, random_seed=None,
-            use_best_model=None, verbose=None, silent=None, logging_level=None, metric_period=None,
-            ctr_leaf_count_limit=None, store_all_simple_ctr=None, max_ctr_complexity=None, has_time=None,
-            allow_const_label=None, one_hot_max_size=None, random_strength=None, name=None, ignored_features=None,
-            train_dir=None, custom_metric=None, eval_metric=None, bagging_temperature=None, save_snapshot=None,
-            snapshot_file=None, fold_len_multiplier=None, used_ram_limit=None, gpu_ram_part=None,
-            pinned_memory_size=None, allow_writing_files=None, final_ctr_computation_mode=None,
-            approx_on_full_history=None, boosting_type=None, simple_ctr=None, combinations_ctr=None,
-            per_feature_ctr=None, ctr_description=None, task_type=None, device_config=None, devices=None,
-            bootstrap_type=None, subsample=None, max_depth=None, n_estimators=None, num_boost_round=None,
-            num_trees=None, colsample_bylevel=None, random_state=None, reg_lambda=None, objective=None, eta=None,
-            max_bin=None, gpu_cat_features_storage=None, data_partition=None, metadata=None):
-        silent = True
-        _CatBoostRegressor.__init__(
-            self, iterations, learning_rate, depth, l2_leaf_reg, model_size_reg, rsm, loss_function,
-            border_count, feature_border_type, fold_permutation_block_size, od_pval, od_wait, od_type,
-            nan_mode, counter_calc_method, leaf_estimation_iterations, leaf_estimation_method,
-            thread_count, random_seed, use_best_model, verbose, silent, logging_level, metric_period,
-            ctr_leaf_count_limit, store_all_simple_ctr, max_ctr_complexity, has_time, allow_const_label,
-            one_hot_max_size, random_strength, name, ignored_features, train_dir, custom_metric,
-            eval_metric, bagging_temperature, save_snapshot, snapshot_file, fold_len_multiplier,
-            used_ram_limit, gpu_ram_part, pinned_memory_size, allow_writing_files,
-            final_ctr_computation_mode, approx_on_full_history, boosting_type, simple_ctr,
-            combinations_ctr, per_feature_ctr, ctr_description, task_type, device_config, devices,
-            bootstrap_type, subsample, max_depth, n_estimators, num_boost_round, num_trees,
-            colsample_bylevel, random_state, reg_lambda, objective, eta, max_bin, gpu_cat_features_storage,
-            data_partition, metadata)
+    def __init__(self, **kwargs):
+        # silent = True
+        _CatBoostRegressor.__init__(self, **kwargs)
         BaseWrapperReg.__init__(self)
 
     @property
