caa87996a38c3eaab63e1e35d227ba4ae3c2c10f
demetoir <wnsqlehlswk@naver.com>
2018-08-17 19:09:50 +0900

    update bench_code.py

--- script/workbench/bench_code.py
+++ script/workbench/bench_code.py
@@ -2,11 +2,11 @@
 import numpy as np
 import pandas as pd
 
-from script.data_handler.samsun_contest import path_head, samsung_contest
+from script.data_handler.samsun_contest import samsung_contest
 from script.util.Logger import pprint_logger, Logger
 from script.util.deco import deco_timeit
+
 # print(built-in function) is not good for logging
-from script.workbench.samsung_inference import SamsungInference
 
 bprint = print
 logger = Logger('bench_code', level='INFO', )
@@ -35,6 +35,7 @@ def print_df_unique(df, key=None):
 
 def exploit_data():
     dataset = samsung_contest(caching=True)
+    path_head = "./data/dataset_kor"
     dataset.load(path_head)
     trainset = dataset.pack['full_set']
     df = trainset.to_DataFrame()
@@ -82,45 +83,9 @@ def exploit_data():
     print(cols)
 
 
-def test_transform():
-    dataset = samsung_contest(caching=False)
-    dataset.load(path_head)
-    trainset = dataset.pack['full_set']
-    df = trainset.to_DataFrame()
-    print(list(df.columns))
-    x_keys = ['c00_주야', 'c01_요일', 'c02_사망자수', 'c03_사상자수', 'c04_중상자수', 'c05_경상자수', 'c06_부상신고자수', 'c07_발생지시도',
-              'c08_발생지시군구',
-              # 'c09_사고유형_대분류',
-              'c10_사고유형_중분류', 'c11_법규위반', 'c13_도로형태', 'c12_도로형태_대분류', 'c14_당사자종별_1당_대분류',
-              'c15_당사자종별_2당_대분류']
-    y_keys = ['c09_사고유형_대분류']
-    trainset.x_keys = x_keys
-    trainset.y_keys = y_keys
-    print(df.info())
-    print(df.head())
-
-    x, y = trainset.next_batch(10)
-    print(x, y)
-    print(x.shape, y.shape)
-
-    x, y = trainset.next_batch(50, balanced_class=True)
-    print(x, y)
-    print(x.shape, y.shape)
-
-    x, y = trainset.full_batch()
-    print(trainset.classes)
-    print(trainset.n_classes)
-    print(trainset.size_group_by_class)
-    print(x, y)
-    print(x.shape, y.shape)
-    print(trainset)
-
-    a_set, b_set = trainset.split()
-    print(a_set)
-    print(b_set)
 
 
 @deco_timeit
 def main():
-    SamsungInference().pipeline()
+    # SamsungInference().pipeline()
     pass
