3346e823f708d6223065fc768d17a5955b7e11ac
demetoir <wnsqlehlswk@naver.com>
2018-08-17 19:00:52 +0900

    remake ParamOpt

--- script/sklearn_like_toolkit/param_search/ParamOpt.py
+++ script/sklearn_like_toolkit/param_search/ParamOpt.py
@@ -3,7 +3,6 @@ from env_settting import SKLEARN_PARAMS_SAVE_PATH
 from script.data_handler.Base.BaseDataset import BaseDataset
 from script.sklearn_like_toolkit.param_search.HyperOpt.HyperOpt import HyperOpt, HyperOpt_fn
 from script.sklearn_like_toolkit.warpper.base.MixIn import DFEncoderMixIn, YLabelOneHotConvertMixIn
-from script.sklearn_like_toolkit.warpper.wrapperGridSearchCV import wrapperGridSearchCV
 from script.util.MixIn import LoggerMixIn
 
 
@@ -11,36 +10,34 @@ class SupervisedHyperOptCVFunc(HyperOpt_fn):
     @staticmethod
     def fn(params, feed_args, feed_kwargs):
         clf_cls = feed_kwargs['clf_cls']
-        dataset = feed_kwargs['dataset']
+        data_set = feed_kwargs['dataset']
         cv = feed_kwargs['cv']
-        metric = feed_kwargs['metric']
+        # TODO add metric func
+        # metric = feed_kwargs['metric']
         split_rate = feed_kwargs['split_rate']
 
         scores = []
         for i in range(cv):
-            dataset.shuffle()
-            train_set, test_set = dataset.split(ratio=split_rate)
-            train_Xs, train_Ys = train_set.full_batch(['Xs', 'Ys'])
-            test_Xs, test_Ys = test_set.full_batch(['Xs', 'Ys'])
+            data_set.shuffle()
+            train_set, test_set = data_set.split(ratio=split_rate)
+            train_x, train_y = train_set.full_batch()
+            test_x, test_y = test_set.full_batch()
 
             clf = clf_cls(**params)
-            clf.fit(train_Xs, train_Ys)
-            score = clf.score(test_Xs, test_Ys)
+            clf.fit(train_x, train_y)
+            score = clf.score(test_x, test_y)
             scores += [score]
 
-        return np.mean(scores)
+        scores = np.array(scores)
+        mean_score = np.mean(scores)
+
+        return {'loss': mean_score, 'losses': scores}
 
 
 class ParamOpt(DFEncoderMixIn, YLabelOneHotConvertMixIn, LoggerMixIn):
-    """
-    Hyperopt
-    grid_serach
-    random_search
-    """
 
-    def __init__(
-            self, func=None, cv=3, n_iter=None, opt_method='HyperOpt', min_best=True,
-            n_job=1, x_df_encoder=None, y_df_encoder=None, clone=True, verbose=0):
+    def __init__(self, func=None, cv=3, n_iter=None, opt_method='HyperOpt', min_best=True,
+                 split_rate=(7, 3), n_jobs=1, x_df_encoder=None, y_df_encoder=None, clone=True, verbose=0):
         DFEncoderMixIn.__init__(self, x_df_encoder, y_df_encoder)
         YLabelOneHotConvertMixIn.__init__(self)
         LoggerMixIn.__init__(self, verbose)
@@ -53,12 +50,12 @@ class ParamOpt(DFEncoderMixIn, YLabelOneHotConvertMixIn, LoggerMixIn):
         self.opt_method = opt_method
         self.opt_funcs = {
             'HyperOpt': self.HyperOptSearchCV,
-            'GridSearch': self.gridSearchCV
         }
         self.n_iter = n_iter
         self.min_best = min_best
-        self.n_jobs = n_job
+        self.n_jobs = n_jobs
         self.clone = clone
+        self.split_rate = split_rate
 
         self.optimizers = {}
         self.optimize_result = {}
@@ -67,8 +64,7 @@ class ParamOpt(DFEncoderMixIn, YLabelOneHotConvertMixIn, LoggerMixIn):
     def _refit(self, clf, x, y, **kwargs):
         dataset = BaseDataset(x=x, y=y)
         dataset.shuffle()
-        train, _ = dataset.split()
-        train_x, train_y = train.full_batch()
+        train_x, train_y = dataset.full_batch()
 
         clf.fit(train_x, train_y)
         return clf
@@ -83,7 +79,7 @@ class ParamOpt(DFEncoderMixIn, YLabelOneHotConvertMixIn, LoggerMixIn):
 
         x, y = self._if_df_encode(x, y)
         y = self.np_arr_to_index(y)
-        dataset = BaseDataset(x=x, y=y)
+        data_set = BaseDataset(x=x, y=y)
 
         opt = HyperOpt(min_best, n_jobs=self.n_jobs)
 
@@ -99,7 +95,10 @@ class ParamOpt(DFEncoderMixIn, YLabelOneHotConvertMixIn, LoggerMixIn):
             feed_args=(),
             feed_kwargs={
                 'clf_cls': clf.__class__,
-                'dataset': dataset
+                'dataset': data_set,
+                'cv': self.cv,
+                'metric': None,
+                'split_rate': self.split_rate
             },
             min_best=min_best
         )
@@ -109,38 +108,18 @@ class ParamOpt(DFEncoderMixIn, YLabelOneHotConvertMixIn, LoggerMixIn):
 
         return self._refit(clf_optimized, x, y)
 
-    def gridSearchCV(self, clf, x, y, **kwargs):
-        x, y = self._if_df_encode(x, y)
-        y = self.np_arr_to_index(y)
-
-        optimizer = wrapperGridSearchCV(clf, clf.tuning_grid, scoring=None, n_jobs=self.n_jobs, cv=self.cv)
-        optimizer.fit(x, y)
-
-        self.optimize_result = optimizer.cv_results_
-        return optimizer.best_estimator_
+    def RandomSearchCv(self, clf, x, y, **kwargs):
+        # TODO
+        raise NotImplementedError
 
     def _fit(self, clf, x, y, **kwargs):
         x, y = self._if_df_encode(x, y)
         y = self.np_arr_to_index(y)
 
         opt_func = self.opt_funcs[self.opt_method]
-        clf_optimized = opt_func(clf, x, y, *kwargs)
+        clf_optimized = opt_func(clf, x, y, **kwargs)
 
         return clf_optimized
-
-        # def fit_pack(self, clf_pack, x, y, **kwargs):
-
-    #     x, y = self._if_df_encode(x, y)
-    #     y = self.np_arr_to_index(y)
-    #
-    #     opt_func = self.opt_funcs[self.opt_method]
-    #     pack = clf_pack.pack
-    #
-    #     params_pack = {}
-    #     for clf_name, clf_ in clf.pack.items():
-    #         print(f'optimize {clf_name}')
-    #         clf_optimized = opt_func(clf, x, y, *kwargs)
-    #         params_pack[clf_name] = clf_optimized.get_paramsa()
 
     def fit(self, clf, x, y, **kwargs):
         return self._fit(clf, x, y, **kwargs)
