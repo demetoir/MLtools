891828a1149b6c3a1bc43c20ae6d6f0c85084b90
demetoir <wnsqlehlswk@naver.com>
2018-08-28 01:27:24 +0900

    update bench_code.py

--- script/workbench/bench_code.py
+++ script/workbench/bench_code.py
@@ -1,15 +1,13 @@
 # -*- coding:utf-8 -*-
-from abc import ABC
 
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+
 from script.data_handler.TGS_salt import TRAIN_IMAGE_PATH, collect_images, TRAIN_MASK_PATH, RLE_mask_encoding, \
     TEST_IMAGE_PATH
 from script.model.sklearn_like_model.BaseModel import BaseModel
-from script.model.sklearn_like_model.Mixin import Ys_MixIn, Xs_MixIn, supervised_metricMethodMixIn, scoreMethodMixIn, \
-    predict_probaMethodMixIn, predictMethodMixIn, supervised_trainMethodMixIn
 from script.util.Logger import pprint_logger, Logger
 from script.util.deco import deco_timeit
 from script.util.PlotTools import PlotTools
@@ -283,149 +281,8 @@ def image_augmentation():
     pass
 
 
-import tensorflow as tf
-from script.util.Stacker import Stacker
-from script.util.tensor_ops import *
-
-
 class learning_rate_mixIn:
     pass
-
-
-class UNet(
-    BaseModel,
-    Xs_MixIn,
-    Ys_MixIn,
-    supervised_trainMethodMixIn,
-    predictMethodMixIn,
-    predict_probaMethodMixIn,
-    scoreMethodMixIn,
-    supervised_metricMethodMixIn
-):
-
-    def __init__(self, verbose=10, net_shapes=(), learning_rate=0.001, learning_rate_decay_rate=0.99,
-                 learning_rate_decay_method=None, beta1=0.01, batch_size=100, **kwargs):
-        BaseModel.__init__(self, verbose, **kwargs)
-        Xs_MixIn.__init__(self)
-        Ys_MixIn.__init__(self)
-        supervised_trainMethodMixIn.__init__(self, None)
-        predictMethodMixIn.__init__(self)
-        predict_probaMethodMixIn.__init__(self)
-        scoreMethodMixIn.__init__(self)
-        supervised_metricMethodMixIn.__init__(self)
-
-        self.net_shapes = net_shapes
-        self.learning_rate = learning_rate
-        self.learning_rate_decay_method = learning_rate_decay_method
-        self.learning_rate_decay_rate = learning_rate_decay_rate
-        self.beta1 = beta1
-        self.batch_size = batch_size
-
-    def _build_input_shapes(self, shapes):
-        ret = {}
-        ret.update(self._build_Xs_input_shape(shapes))
-        ret.update(self._build_Ys_input_shape(shapes))
-
-    def _build_main_graph(self):
-        self.Xs = tf.placeholder(tf.float32, self.Xs_shape, name='Xs')
-        self.Ys = tf.placeholder(tf.float32, self.Ys_shape, name='Ys')
-        self.lr_ph = tf.placeholder(tf.float32, 1, name='lr_ph')
-
-        # todo
-        stage_1 = 128
-        stage_2 = 64
-        stage_3 = 32
-
-        stack = Stacker(self.Xs)
-        # encoder
-        stack.conv_block(64, CONV_FILTER_3311, relu)
-        stack.conv_block(64, CONV_FILTER_3311, relu)
-        stage_1_concat = stack.last_layer
-        stack.max_pooling(CONV_FILTER_2211)
-
-        stack.conv_block(128, CONV_FILTER_3311, relu)
-        stack.conv_block(128, CONV_FILTER_3311, relu)
-        stage_2_concat = stack.last_layer
-        stack.max_pooling(CONV_FILTER_2211)
-
-        stack.conv_block(256, CONV_FILTER_3311, relu)
-        stack.conv_block(256, CONV_FILTER_3311, relu)
-        stage_3_concat = stack.last_layer
-        stack.max_pooling(CONV_FILTER_2211)
-
-        stack.conv_block(512, CONV_FILTER_3311, relu)
-        stack.conv_block(512, CONV_FILTER_3311, relu)
-        stage_3_concat = stack.last_layer
-        stack.max_pooling(CONV_FILTER_2211)
-
-        # low stage
-        stack.conv_block(1024, CONV_FILTER_3311, relu)
-        stack.conv_block(1024, CONV_FILTER_3311, relu)
-
-        # decoder
-        stack.concat(stage_3, axis=3)
-        stack.conv_block(512, CONV_FILTER_3311, relu)
-        stack.conv_block(512, CONV_FILTER_3311, relu)
-        stack.conv2d_transpose(())
-
-
-
-        pass
-
-    def _build_loss_function(self):
-        # todo
-
-        pass
-
-    def _build_train_ops(self):
-        # todo
-
-        pass
-
-    @property
-    def train_ops(self):
-        return None
-
-    @property
-    def predict_ops(self):
-        pass
-
-    @property
-    def predict_proba_ops(self):
-        pass
-
-    @property
-    def score_ops(self):
-        pass
-
-    @property
-    def metric_ops(self):
-        pass
-
-    def train(self, Xs, Ys, epoch=1, save_interval=None, batch_size=None):
-
-        self._prepare_train(Xs=Xs, Ys=Ys)
-        dataset = self.to_dummyDataset(Xs=Xs, Ys=Ys)
-
-        if batch_size is None:
-            batch_size = self.batch_size
-
-        iter_num = 0
-        iter_per_epoch = dataset.size
-        for e in trange(epoch):
-            for i in range(iter_per_epoch):
-                iter_num += 1
-
-                Xs, Ys = dataset.next_batch(batch_size, batch_keys=['Xs', 'Ys'])
-                self.sess.run(self.train_ops, feed_dict={self._Xs: Xs, self._Ys: Ys})
-
-            Xs, Ys = dataset.next_batch(batch_size, batch_keys=['Xs', 'Ys'], update_cursor=False)
-            loss = self.sess.run(self.metric_ops, feed_dict={self._Xs: Xs, self._Ys: Ys})
-            self.log.info(f"e:{e}, i:{iter_num} loss : {loss}")
-
-            if save_interval is not None and e % save_interval == 0:
-                self.save()
-        pass
 
 
 class FusionNet(BaseModel):
