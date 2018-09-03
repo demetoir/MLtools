4239b5e1ea1b689d895ef6181679901b2f773f11
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-04 01:56:25 +0900

    fix test

--- unit_test/model/sklearn_like_model/net_structure/test_VGG16Structure.py
+++ unit_test/model/sklearn_like_model/net_structure/test_VGG16Structure.py
@@ -1,8 +1,15 @@
+from tensorflow.contrib import slim
+
 from script.model.sklearn_like_model.net_structure.VGG16Structure import VGG16Structure
 import tensorflow as tf
 import numpy as np
 
 from script.util.deco import deco_timeit
+
+
+def show_summary():
+    model_vars = tf.trainable_variables()
+    slim.model_analyzer.analyze_vars(model_vars, print_info=True)
 
 
 @deco_timeit
@@ -18,3 +25,4 @@ def test_VGG16Structure():
         sess.run(tf.global_variables_initializer())
         forward = sess.run(h, feed_dict={x: x_np})
         print(forward.shape)
+        show_summary()
