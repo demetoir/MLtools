68cde230d2819375033c860835f5c59cf0461c8d
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-04 02:17:38 +0900

    refactoring, add mixin, fix bug

--- script/model/sklearn_like_model/net_structure/Base_net_structure.py
+++ script/model/sklearn_like_model/net_structure/Base_net_structure.py
@@ -1,22 +1,22 @@
+from script.util.MixIn import LoggerMixIn
 from script.util.tensor_ops import collect_vars, join_scope, get_scope
 
 
-class Base_net_structure:
-    def __init__(self, reuse=False, name='Base_net_structure'):
+class Base_net_structure(LoggerMixIn):
+    def __init__(self, reuse=False, name='Base_net_structure', verbose=0):
+        super().__init__(verbose)
         self.reuse = reuse
         self.name = name
         self._vars = None
 
-    def get_vars(self):
+    @property
+    def vars(self):
         if self._vars is None:
-            self._vars = collect_vars(join_scope(get_scope(), self.name))
+            if len(get_scope()) == 0:
+                self._vars = collect_vars(self.name)
+            else:
+                self._vars = collect_vars(join_scope(get_scope(), self.name))
         return self._vars
 
     def build(self):
         raise NotImplementedError
-
-
-# TODO
-# TODO add logger MixIn
-def test_net_structure_build():
-    pass
