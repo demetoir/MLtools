9663c39de6ccfb7c848fbf344913f27569867bd7
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-05 02:59:27 +0900

    add callback test

--- unit_test/model/sklearn_like_model/test_UNet.py
+++ unit_test/model/sklearn_like_model/test_UNet.py
@@ -1,11 +1,12 @@
-from pprint import pprint
-
 import numpy as np
-
+from pprint import pprint
+from script.data_handler.ImgMaskAug import ActivatorMask, ImgMaskAug
 from script.data_handler.TGS_salt import HEAD_PATH, collect_images
+from script.model.sklearn_like_model.BaseModel import BaseDatasetCallback
 from script.model.sklearn_like_model.UNet import UNet
 from script.util.PlotTools import PlotTools
 from script.util.misc_util import path_join
+from imgaug import augmenters as iaa
 
 plot = PlotTools(save=True, show=False)
 
@@ -77,7 +78,7 @@ def test_UNet():
     print(y_encode.shape)
 
     Unet = UNet(stage=4, batch_size=7)
-    Unet.train(x, y_encode, epoch=100)
+    Unet.train(x, y_encode, epoch=100, early_stop=True, patience=10)
 
     score = Unet.score(x, y_encode)
     pprint(score)
@@ -133,3 +134,74 @@ def test_train_early_stop():
     y = None
     model = dummy_model()
     train_early_stop(model, x, y, n_epoch=100)
+
+
+class dataset_callback(BaseDatasetCallback):
+    def __init__(self, x, y, batch_size):
+        super().__init__(x, y, batch_size)
+
+        self.seq = iaa.Sequential([
+            iaa.Fliplr(0.5, name="Flipper"),
+
+        ])
+        self.activator = ActivatorMask([])
+        self.aug = ImgMaskAug(self.x, self.y, self.seq, self.activator, self.batch_size, n_jobs=1)
+
+    @property
+    def size(self):
+        return len(self.x)
+
+    def shuffle(self):
+        pass
+
+    def next_batch(self, batch_size, batch_keys=None, update_cursor=True, balanced_class=False, out_type='concat'):
+        x, y = self.aug.get_batch()
+        return x, y
+
+
+def test_train_dataset_callback():
+    sample_IMAGE_PATH = path_join(HEAD_PATH, 'sample/images')
+    sample_MASK_PATH = path_join(HEAD_PATH, 'sample/masks')
+
+    sample_size = 7
+    limit = None
+    print(f'collect sample images')
+    train_images, _, _ = collect_images(sample_IMAGE_PATH, limit=limit)
+    train_images = train_images.reshape([-1, 101, 101])
+    print(f'collect sample images')
+    train_mask_images, _, _ = collect_images(sample_MASK_PATH, limit=limit)
+    train_mask_images = train_mask_images.reshape([-1, 101, 101])
+    x = train_images
+    y = train_mask_images
+
+    import cv2
+
+    x = np.array([cv2.resize(a, (128, 128)) for a in x]).reshape([-1, 128, 128, 1])
+    y = np.array([cv2.resize(a, (128, 128)) for a in y]).reshape([-1, 128, 128, 1])
+    y_gt = y
+
+    y_encode = mask_label_encoder.to_label(y)
+    print(x.shape)
+    print(y_encode.shape)
+
+    Unet = UNet(stage=4, batch_size=7)
+    # Unet.train(x, y_encode, epoch=100)
+    Unet.train(x, y_encode, epoch=1000, dataset_callback=dataset_callback)
+    Unet.train(x, y_encode, epoch=1000, dataset_callback=dataset_callback)
+
+    score = Unet.score(x, y_encode)
+    pprint(score)
+
+    predict = Unet.predict(x)
+    pprint(predict[0])
+    pprint(predict.shape)
+
+    proba = Unet.predict_proba(x)
+    pprint(proba[0])
+    pprint(proba.shape)
+
+    metric = Unet.metric(x, y_encode)
+    print(metric)
+
+    predict = mask_label_encoder.from_label(predict)
+    plot.plot_image_tile(np.concatenate([x, predict, y_gt], axis=0), title='predict', column=sample_size)
