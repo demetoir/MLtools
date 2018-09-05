9a83772049aab300d9bd00c39fec8c9cc57eee46
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-05 21:35:04 +0900

    add arg

--- script/model/sklearn_like_model/net_structure/UNetStructure.py
+++ script/model/sklearn_like_model/net_structure/UNetStructure.py
@@ -1,18 +1,19 @@
 import tensorflow as tf
+
 from script.model.sklearn_like_model.net_structure.Base_net_structure import Base_net_structure
 from script.util.Stacker import Stacker
-from script.util.tensor_ops import CONV_FILTER_3311, relu, CONV_FILTER_2211, pixel_wise_softmax, CONV_FILTER_2222
+from script.util.tensor_ops import CONV_FILTER_3311, relu, CONV_FILTER_2211, CONV_FILTER_2222
 
 
 class UNetStructure(Base_net_structure):
 
-    def __init__(self, x, level=4, n_classes=2, reuse=False, name=None, verbose=0):
+    def __init__(self, x, level=4, n_channel=64, n_classes=2, reuse=False, name=None, verbose=0):
         super().__init__(reuse, name, verbose)
 
         self.x = x
         self.level = level
         self.n_classes = n_classes
-        self.n_channel = 64
+        self.n_channel = n_channel
 
     def build(self):
         def _Unet_recursion(stacker, n_channel, level):
