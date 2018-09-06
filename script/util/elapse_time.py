8ce28f9cbeca58845dac25e996fdf79b8f37d758
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-07 01:47:57 +0900

    fix logging

--- script/util/elapse_time.py
+++ script/util/elapse_time.py
@@ -5,11 +5,13 @@ class elapse_time:
     def __init__(self, title=None):
         self.start_time = time.time()
         self.title = title
+        if title is None:
+            self.title = ''
+        else:
+            self.title = title
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.title:
-            print(self.title)
-        print(f"time {time.time() - self.start_time:.4f}'s elapsed")
+        print(f"{self.title}, time {time.time() - self.start_time:.4f}'s elapsed")
         return True
 
     def __enter__(self):
