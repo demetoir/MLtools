e29321eb9085825ec882844f41c9932a4859689a
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-07 02:14:16 +0900

    fix exception handling

--- script/util/elapse_time.py
+++ script/util/elapse_time.py
@@ -12,7 +12,13 @@ class elapse_time:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print(f"{self.title}, time {time.time() - self.start_time:.4f}'s elapsed")
-        return True
+        if (exc_type, exc_val, exc_tb) == (None, None, None):
+            return True
+        else:
+            # print(exc_type)
+            # print(exc_val)
+            print(exc_tb)
+            return False
 
     def __enter__(self):
         return None
