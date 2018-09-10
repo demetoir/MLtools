c36ea3d6bb8583b9f2e312fc9f0ed754d1962f96
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-11 01:18:53 +0900

    fix Top_k_save.py log add error handling Top_k_save.py

--- script/model/sklearn_like_model/Top_k_save.py
+++ script/model/sklearn_like_model/Top_k_save.py
@@ -1,10 +1,8 @@
 import os
+import numpy as np
 import shutil
 from pprint import pprint
-
-import numpy as np
-
-from script.util.misc_util import path_join, load_json, setup_directory, dump_json
+from script.util.misc_util import path_join, load_json, setup_directory, dump_json, error_trace
 
 
 class Top_k_save:
@@ -25,36 +23,41 @@ class Top_k_save:
                 setup_directory(path_join(self.path, f'top_{i}'))
 
     def __call__(self, metric, model):
-        for i in reversed(range(1, self.k + 1)):
-            if self.top_k[i - 1] >= metric >= self.top_k[i]:
-                # update top_k
-                self.top_k.insert(i, metric)
-                self.top_k.pop(self.k + 1)
+        try:
+            for i in reversed(range(1, self.k + 1)):
+                if self.top_k[i - 1] >= metric >= self.top_k[i]:
+                    # update top_k
+                    self.top_k.insert(i, metric)
+                    self.top_k.pop(self.k + 1)
 
-                # dump top_k json
-                dump_json(self.top_k, path_join(self.path, 'top_k.json'))
+                    # dump top_k json
+                    dump_json(self.top_k, path_join(self.path, 'top_k.json'))
 
-                print(f'update top_k at {i}th, metric = {metric}')
-                pprint(self.top_k)
+                    print(f'update top_k at {i}th, metric = {metric}')
+                    pprint(self.top_k[1:])
 
-                if self.save_model:
-                    # del worst dir
-                    shutil.rmtree(path_join(self.path, f'top_{self.k}'))
+                    if self.save_model:
+                        # del worst dir
+                        shutil.rmtree(path_join(self.path, f'top_{self.k}'))
 
-                    # shift dir
-                    path_pairs = [
-                        (
-                            path_join(self.path, f'top_{idx}'),
-                            path_join(self.path, f'top_{idx+1}')
-                        )
-                        for idx in range(i, self.k)
-                    ]
-                    path_pairs = list(reversed(path_pairs))
-                    for src, dst in path_pairs:
-                        os.rename(src, dst)
+                        # shift dir
+                        path_pairs = [
+                            (
+                                path_join(self.path, f'top_{idx}'),
+                                path_join(self.path, f'top_{idx+1}')
+                            )
+                            for idx in range(i, self.k)
+                        ]
+                        path_pairs = list(reversed(path_pairs))
+                        for src, dst in path_pairs:
+                            os.rename(src, dst)
 
-                    # save model
-                    save_path = path_join(self.path, f'top_{i}')
-                    model.save(save_path)
+                        # save model
+                        save_path = path_join(self.path, f'top_{i}')
+                        model.save(save_path)
 
-                break
+                    break
+        except BaseException as e:
+            print(error_trace(e))
+            raise RuntimeError(f'while Top k save, raise {e}')
+
