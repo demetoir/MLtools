140b2d386706a206e2cca5d4926a05ff64491b7a
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-19 02:23:56 +0900

    fix Epoch_callback SS_Inference.py

--- script/workbench/TGS_salt/SS_Inference.py
+++ script/workbench/TGS_salt/SS_Inference.py
@@ -15,7 +15,7 @@ INSTANCE_PATH = f'./instance/TGS_salt/SS'
 PLOT_PATH = f'./matplot/TGS_salt/SS'
 
 
-class Unet_epoch_callback(BaseEpochCallback):
+class Epoch_callback(BaseEpochCallback):
     def __init__(self, model, test_x, test_y, params):
         super().__init__()
         self.model = model
@@ -32,21 +32,25 @@ class Unet_epoch_callback(BaseEpochCallback):
 
         self.top_k_save = Top_k_save(path_join(INSTANCE_PATH, self.run_id), k=3)
 
-    def log_TGS_salt_metric(self, dataset, epoch):
-        x, y = dataset.next_batch(1000)
-        x = x.reshape([-1, 101, 101, 1])
-        y = y.reshape([-1, 101, 101, 1])
-        predict_train = self.model.predict(x)
-        predict_train = mask_label_encoder.from_label(predict_train)
-        predict_test = self.model.predict(self.test_x)
-        predict_test = mask_label_encoder.from_label(predict_test)
-        self.train_loss = self.model.metric(x, y)
-        self.train_score = TGS_salt_metric(y, predict_train)
-        self.test_score = TGS_salt_metric(self.test_y, predict_test)
-        tqdm.write(f'e:{epoch}, TGS_salt_metric train = {self.train_score}\n'
-                   f'test = {self.test_score}\n')
+    def log_TGS_salt_metric(self, epoch):
+        msg = f'\n'
+        msg += f'e:{epoch}, '
+        msg += f'TGS_salt_metric train score = {self.train_score}\n'
+        msg += f'TGS_salt_metric test score = {self.test_score}\n'
+        msg += f'empty mask score\n'
+        msg += f'\n'
+        msg += f'train empty mask score = {self.train_empty_mask_score}, (total = {self.train_empty_mask_size})\n'
+        msg += f'test empty mask score = {self.test_empty_mask_score}, (total = {self.test_empty_mask_size})\n'
+        msg += f'\n'
+        msg += f'train non empty mask score = {self.train_non_empty_score},' \
+               f'(total = {self.train_non_empty_mask_size}\n'
+        msg += f'test non empty mask score = {self.test_non_empty_score}' \
+               f'(total = {self.test_non_empty_mask_size})\n'
+        msg += f'\n'
 
-    def plot_mask(self, dataset, epoch):
+        tqdm.write(msg)
+
+    def plot_mask_image(self, dataset, epoch):
         x, y = dataset.next_batch(20)
         x = x.reshape([-1, 101, 101, 1])
         y = y.reshape([-1, 101, 101, 1]) * 254
@@ -55,7 +59,7 @@ class Unet_epoch_callback(BaseEpochCallback):
         proba = proba[:, :, :, 1].reshape([-1, 101, 101, 1]) * 255
         predict = mask_label_encoder.from_label(predict)
 
-        def f(*args, size=10):
+        def scramble_column(*args, size=10):
             ret = []
             for i in range(0, len(args[0]), size):
                 for j in range(len(args)):
@@ -63,21 +67,109 @@ class Unet_epoch_callback(BaseEpochCallback):
 
             return np.concatenate(ret, axis=0)
 
-        np_tile = f(x, y, predict, proba)
+        np_tile = scramble_column(x, y, predict, proba)
         self.plot.plot_image_tile(
             np_tile,
             title=f'predict_epoch({epoch})',
             column=10,
-            path=path_join(PLOT_PATH, self.run_id, f'predict_epoch({epoch}).png'))
+            path=path_join(PLOT_PATH, self.run_id, f'predict_mask/({epoch}).png'))
+
+    def plot_mask_rate_iou(self, dataset, epoch):
+        test_x = self.test_x[:100]
+        test_y = self.test_y[:100]
+        train_x, train_y = dataset.next_batch(100)
+
+        train_predict = self.model.predict(train_x)
+
+        def iou(true, predict):
+            if np.sum(true) == 0:
+                if np.sum(predict) > 0:
+                    return 0
+                else:
+                    return 1
+            else:
+                mask_true = true / 255
+                mask_predict = predict / 255
+
+                intersect = np.logical_and(mask_true, mask_predict)
+                union = np.logical_or(mask_true, mask_predict)
+                iou = np.sum(intersect) / np.sum(union)
+
+                return iou
+            pass
+
+        def mask_rate(mask):
+            pass
+
+        x = mask_rate(train_y)
+        y = iou(train_y, train_predict)
+        dots = zip(x, y)
+        self.plot.scatter_2d(
+            dots,
+            title=f'mask rate and iou',
+            path=path_join(PLOT_PATH, self.run_id, f'mask_rate_iou/({epoch}).png'))
 
     def update_summary(self, sess, epoch):
         self.summary_train_loss.update(sess, self.train_loss, epoch)
         self.summary_train_acc.update(sess, self.train_score, epoch)
         self.summary_test_acc.update(sess, self.test_score, epoch)
 
+    def update_data(self, sess, dataset, epoch):
+        train_x, train_y = dataset.next_batch(1000)
+        self.train_x = train_x.reshape([-1, 101, 101, 1])
+        self.train_y = train_y.reshape([-1, 101, 101, 1])
+
+        train_predict = self.model.predict(self.train_x)
+        self.train_predict = mask_label_encoder.from_label(train_predict)
+
+        test_predict = self.model.predict(self.test_x)
+        self.test_predict = mask_label_encoder.from_label(test_predict)
+
+        self.train_loss = self.model.metric(self.train_x, self.train_y)
+
+        self.train_score = TGS_salt_metric(self.train_y, self.train_predict)
+        self.test_score = TGS_salt_metric(self.test_y, self.test_predict)
+
+        def get_idxs(true):
+            size = len(true)
+            true = true.reshape([size, -1])
+            true = np.mean(true, axis=1)
+
+            empty_idxs = true == 0
+            non_empty_idxs = true != 0
+            return empty_idxs, non_empty_idxs
+
+        train_empty_idxs, train_non_empty_idxs = get_idxs(self.train_y)
+        self.train_empty_mask_size = len(train_empty_idxs)
+        self.train_non_empty_mask_size = len(train_non_empty_idxs)
+
+        self.train_empty_mask_predict = self.train_predict[train_empty_idxs]
+        self.train_non_empty_mask_predict = self.train_predict[train_non_empty_idxs]
+
+        self.train_empty_mask_y = self.train_y[train_empty_idxs]
+        self.train_non_empty_mask_y = self.train_y[train_non_empty_idxs]
+
+        self.train_empty_mask_score = TGS_salt_metric(self.train_empty_mask_y, self.train_empty_mask_predict)
+        self.train_non_empty_score = TGS_salt_metric(self.train_non_empty_mask_y, self.train_non_empty_mask_predict)
+
+        test_empty_idxs, test_non_empty_idxs = get_idxs(self.test_y)
+        self.test_empty_mask_size = len(test_empty_idxs)
+        self.test_non_empty_mask_size = len(test_non_empty_idxs)
+
+        self.test_empty_mask_predict = self.test_predict[test_empty_idxs]
+        self.test_non_empty_mask_predict = self.test_predict[test_non_empty_idxs]
+
+        self.test_empty_mask_y = self.test_y[test_empty_idxs]
+        self.test_non_empty_mask_y = self.test_y[test_non_empty_idxs]
+
+        self.test_empty_mask_score = TGS_salt_metric(self.test_empty_mask_y, self.test_empty_mask_predict)
+        self.test_non_empty_score = TGS_salt_metric(self.test_non_empty_mask_y, self.test_non_empty_mask_predict)
+
     def __call__(self, sess, dataset, epoch, log=None):
-        self.plot_mask(dataset, epoch)
-        self.log_TGS_salt_metric(dataset, epoch)
+        self.update_data(sess, dataset, epoch)
+
+        self.plot_mask_image(dataset, epoch)
+        self.log_TGS_salt_metric(epoch)
         self.update_summary(sess, epoch)
         self.top_k_save(self.test_score, self.model)
 
@@ -154,7 +246,7 @@ class SemanticSegmentation_pipeline:
         else:
             model = SemanticSegmentation(**params)
 
-        epoch_callback = Unet_epoch_callback(model, self.test_x, self.test_y, params)
+        epoch_callback = Epoch_callback(model, self.test_x, self.test_y, params)
         dataset_callback = TGS_salt_aug_callback(self.train_x, self.train_y_encode, params['batch_size']) \
             if augmentation else None
 
