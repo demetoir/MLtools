c8e1c41df05d13483c9b178d89b2c0ecf862b635
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-09-24 17:30:30 +0900

    fix FusionNetStructure.py

--- script/model/sklearn_like_model/NetModule/FusionNetStructure.py
+++ script/model/sklearn_like_model/NetModule/FusionNetStructure.py
@@ -49,9 +49,8 @@ class FusionNetModule(BaseNetModule):
 
             return [skip_tensor] + recursive(stacker, n_channel * 2, level - 1)
 
-        with tf.variable_scope('encoder'):
-            self.encoder_stacker = Stacker(x)
-            self.skip_connects = recursive(self.encoder_stacker, n_channel, level)
+        self.encoder_stacker = Stacker(x, name='encoder')
+        self.skip_connects = recursive(self.encoder_stacker, n_channel, level)
 
         return self.encoder_stacker.last_layer, self.skip_connects
 
@@ -74,17 +73,15 @@ class FusionNetModule(BaseNetModule):
             self.conv_seq(stacker, n_channel, depth, dropout_rate_tensor)
 
         skip_connects = skip_connects[::-1]
-        with tf.variable_scope('decoder'):
-            self.decoder_stacker = Stacker(x)
-            recursive(self.decoder_stacker, n_channel, level)
+        self.decoder_stacker = Stacker(x, name='decoder')
+        recursive(self.decoder_stacker, n_channel, level)
 
         return self.decoder_stacker.last_layer
 
     def bottom_layer(self, x, n_channel, depth, dropout_rate_tensor):
-        with tf.variable_scope('bottom_layer'):
-            stacker = Stacker(x)
-            stacker = self.conv_seq(stacker, n_channel, depth, dropout_rate_tensor)
-            self.bottom_layer_stacker = stacker
+        stacker = Stacker(x, name='bottom')
+        stacker = self.conv_seq(stacker, n_channel, depth, dropout_rate_tensor)
+        self.bottom_layer_stacker = stacker
 
         return self.bottom_layer_stacker.last_layer
 
@@ -93,93 +90,30 @@ class FusionNetModule(BaseNetModule):
         self.DynamicDropoutRate = DynamicDropoutRate(self.dropout_rate)
         self.drop_out_tensor = self.DynamicDropoutRate.tensor
 
-        def recursive_build(stacker, n_channel, level, dropout_rate_tensor=None):
-            if level == 0:
-                stacker.conv_block(n_channel, CONV_FILTER_3311, relu)
-
-                for i in range(self.depth):
-                    stacker.add_layer(_residual_block, n_channel, CONV_FILTER_3311, relu)
-                    if dropout_rate_tensor:
-                        stacker.dropout(dropout_rate_tensor)
-
-                stacker.conv_block(n_channel, CONV_FILTER_3311, relu)
-
-            else:
-                # encode
-                stacker.conv_block(n_channel, CONV_FILTER_3311, relu)
-
-                for i in range(self.depth):
-                    stacker.add_layer(_residual_block, n_channel, CONV_FILTER_3311, relu)
-                    if dropout_rate_tensor:
-                        stacker.dropout(dropout_rate_tensor)
-
-                stacker.conv_block(n_channel, CONV_FILTER_3311, relu)
-
-                x_add = stacker.last_layer
-                concat = stacker.last_layer
-
-                stacker.max_pooling(CONV_FILTER_2222)
-
-                # recursion
-                stacker = recursive_build(stacker, n_channel * 2, level - 1)
-
-                # decode
-                stacker.upscale_2x_block(n_channel, CONV_FILTER_2211, relu)
-
-                # TODO hack to dynamic batch size after conv transpose, concat must need. wtf?
-                stacker.concat(concat, axis=3)
-                stacker.conv_block(n_channel, CONV_FILTER_1111, relu)
-
-                stacker.residual_add(x_add)
-
-                stacker.conv_block(n_channel, CONV_FILTER_3311, relu)
-                # if dropout_rate_tensor:
-                #     stacker.dropout(dropout_rate_tensor)
-
-                for i in range(self.depth):
-                    stacker.add_layer(_residual_block, n_channel, CONV_FILTER_3311, relu)
-                    if dropout_rate_tensor:
-                        stacker.dropout(dropout_rate_tensor)
-
-                stacker.conv_block(n_channel, CONV_FILTER_3311, relu)
-                # if dropout_rate_tensor:
-                # stacker.dropout(dropout_rate_tensor)
-
-            return stacker
-
         with tf.variable_scope(self.name, reuse=self.reuse):
-            recursive = False
-            if recursive:
-                self.stacker = Stacker(self.x, verbose=self.verbose)
-                stacker = recursive_build(self.stacker, self.n_channel, self.level, self.drop_out_tensor)
+            encode, skip_tensors = self.encoder(
+                self.x, self.n_channel, self.depth, self.level,
+                self.drop_out_tensor)
 
-                self.logit = stacker.conv_block(self.n_classes, CONV_FILTER_3311, relu)
-                stacker.pixel_wise_softmax()
-                self.proba = stacker.last_layer
-            else:
-                encode, skip_tensors = self.encoder(
-                    self.x, self.n_channel, self.depth, self.level,
-                    self.drop_out_tensor)
+            bottom_layer = self.bottom_layer(
+                encode,
+                self.n_channel * (2 ** (self.level - 1)),
+                self.depth,
+                self.drop_out_tensor
+            )
 
-                bottom_layer = self.bottom_layer(
-                    encode,
-                    self.n_channel * (2 ** (self.level - 1)),
-                    self.depth,
-                    self.drop_out_tensor
-                )
+            decode = self.decoder(
+                bottom_layer,
+                skip_tensors,
+                self.n_channel,
+                self.depth,
+                self.level,
+                self.drop_out_tensor,
+            )
 
-                decode = self.decoder(
-                    bottom_layer,
-                    skip_tensors,
-                    self.n_channel,
-                    self.depth,
-                    self.level,
-                    self.drop_out_tensor,
-                )
-
-                self.decoder_stacker.conv_block(self.n_classes, CONV_FILTER_3311, relu)
-                self.logit = self.decoder_stacker.last_layer
-                self.proba = pixel_wise_softmax(self.logit)
+            self.decoder_stacker.conv_block(self.n_classes, CONV_FILTER_3311, relu)
+            self.logit = self.decoder_stacker.last_layer
+            self.proba = pixel_wise_softmax(self.logit)
 
     def set_train(self, sess):
         self.DynamicDropoutRate.set_train(sess)
