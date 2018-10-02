393f97b027f3aed8c4fc9d40d021c76025672e14
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-10-03 00:33:08 +0900

    add metaclass at callback, BaseModel.py

--- script/model/sklearn_like_model/BaseModel.py
+++ script/model/sklearn_like_model/BaseModel.py
@@ -39,30 +39,63 @@ class BaseDatasetCallback:
         raise NotImplementedError
 
 
-class BaseEpochCallback:
+class metaEpochCallback(type):
+    """Metaclass for hook inherited class's function
+    metaclass ref from 'https://code.i-harness.com/ko/q/11fc307'
+    """
+
+    def __init__(cls, name, bases, cls_dict):
+        type.__init__(cls, name, bases, cls_dict)
+
+        # hook __call__
+        f_name = '__call__'
+        if f_name in cls_dict:
+            func = cls_dict[f_name]
+
+            def new_func(self, model, dataset, metric, epoch, *args, **kwargs):
+                if getattr(self, 'is_trance_on', False):
+                    dc = getattr(self, 'dc')
+                    dc_key = getattr(self, 'dc_key')
+                    metric = getattr(dc, dc_key)
+
+                return func(self, model, dataset, metric, epoch, *args, **kwargs)
+
+            new_func.__name__ = f_name + '_wrap'
+            setattr(cls, f_name, new_func)
+
+        def wrap_return_self(f_name, cls_dict, cls):
+            func = cls_dict[f_name]
+
+            def new_func(self, *args, **kwargs):
+                func(self, *args, **kwargs)
+                return self
+
+            new_func.__name__ = f_name + '_wrap'
+            setattr(cls, f_name, new_func)
+
+        # hook return self
+        f_name = 'trace_on'
+        if f_name in cls_dict:
+            wrap_return_self(f_name, cls_dict, cls)
+
+        f_name = 'trace_off'
+        if f_name in cls_dict:
+            wrap_return_self(f_name, cls_dict, cls)
+
+
+class BaseEpochCallback(metaclass=metaEpochCallback):
     def __call__(self, model, dataset, metric, epoch):
         raise NotImplementedError
 
     def trace_on(self, dc, key):
         self.dc = dc
         self.dc_key = key
+        self.is_trance_on = True
 
-    def hook_metric(self, metric):
-        if hasattr(self, 'dc') and hasattr(self, 'dc_key'):
-            metric = getattr(self.dc, self.dc_key)
-        return metric
-
-    def on_start_epoch(self, model, dataset, metric, epoch):
-        pass
-
-    def on_end_epoch(self, model, dataset, metric, epoch):
-        pass
-
-    def on_start_train(self, model, dataset, metric, epoch):
-        pass
-
-    def on_end_train(self, model, dataset, metric, epoch):
-        pass
+    def trace_off(self):
+        del self.dc
+        del self.dc_key
+        self.is_trance_on = False
 
 
 META_DATA_FILE_NAME = 'instance.meta'
