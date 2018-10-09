86d9eb494d935958e7b9204a6e17b239dd0a6948
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-10-10 06:58:32 +0900

    fix metric, lint BestSave.py

--- script/model/sklearn_like_model/callback/BestSave.py
+++ script/model/sklearn_like_model/callback/BestSave.py
@@ -1,20 +1,20 @@
 import numpy as np
+
 from script.model.sklearn_like_model.callback.BaseEpochCallback import BaseEpochCallback
-from script.util.misc_util import path_join, setup_directory
+from script.util.misc_util import setup_directory
 
 
 class BestSave(BaseEpochCallback):
-    def __init__(self, path, max_best=True, name='top_k_save', log=print):
+    def __init__(self, path, max_best=True, name='BestSave', log=print):
         self.path = path
         self.max_best = max_best
         self.name = name
         self.log = log
 
-        self.top_k_json_path = path_join(self.path, 'top_k.json')
         if self.max_best:
-            self.best_metric = np.Inf
-        else:
             self.best_metric = -np.Inf
+        else:
+            self.best_metric = np.Inf
 
         setup_directory(self.path)
 
