e2c9872cea84f874ba02f11786e77c50bb48e874
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-10-23 05:20:57 +0900

    add AdagradDA.py

---
+++ script/model/sklearn_like_model/NetModule/optimizer/AdagradDA.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+
+from script.model.sklearn_like_model.NetModule.BaseNetModule import BaseNetModule
+from script.model.sklearn_like_model.NetModule.DynamicVariable import DynamicVariable
+
+
+class AdagradDA(BaseNetModule):
+    def __init__(
+            self,
+            loss,
+            train_var_list,
+            learning_rate,
+            global_step,
+            initial_gradient_squared_accumulator_value=0.1,
+            l1_regularization_strength=0.0,
+            l2_regularization_strength=0.0,
+            reuse=False,
+            name=None,
+            verbose=0
+    ):
+        super().__init__(verbose=verbose, name=name, reuse=reuse)
+        self.loss = loss
+        self.global_step = global_step
+        self.initial_gradient_squared_accumulator_value = initial_gradient_squared_accumulator_value
+        self.l1_regularization_strength = l1_regularization_strength
+        self.l2_regularization_strength = l2_regularization_strength
+        self.train_var_list = train_var_list
+        self._learning_rate = learning_rate
+
+        self._lr_module = DynamicVariable(self._learning_rate, name='learning_rate')
+
+    def __str__(self):
+        s = f"{self.__class__.__name__}"
+        s += f"learning rate = {self.learning_rate}\n"
+        s += f"global_step = {self.global_step}\n"
+        s += f"initial_gradient_squared_accumulator_value = {self.initial_gradient_squared_accumulator_value}\n"
+        s += f"l1_regularization_strength = {self.l1_regularization_strength}\n"
+        s += f"l2_regularization_strength = {self.l2_regularization_strength}\n"
+
+        return s
+
+    @property
+    def learning_rate(self):
+        return self._lr_module.value
+
+    def build(self):
+        self.log.info(f'build {self.name}')
+        with tf.variable_scope(self.name):
+            self._lr_module.build()
+
+            self._optimizer = tf.train.AdagradDAOptimizer(
+                learning_rate=self._lr_module.variable,
+                global_step=self.global_step,
+                initial_gradient_squared_accumulator_value=self.initial_gradient_squared_accumulator_value,
+                l1_regularization_strength=self.l1_regularization_strength,
+                l2_regularization_strength=self.l2_regularization_strength,
+            )
+            self._train_op = self.optimizer.minimize(
+                loss=self.loss,
+                var_list=self.train_var_list
+            )
+
+        return self
+
+    def update_learning_rate(self, sess, lr):
+        self._lr_module.update(sess, lr)
+
+    @property
+    def train_op(self):
+        return self._train_op
+
+    @property
+    def optimizer(self):
+        return self._optimizer
