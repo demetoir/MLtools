940f4631ca58618e62a7f26f3e23196e93e9cccf
demetoir_desktop_windows <wnsqlehlswk@gmail.com>
2018-10-30 16:37:20 +0900

    fix, refactoring, ProximalAdagrad.py

--- script/model/sklearn_like_model/NetModule/optimizer/ProximalAdagrad.py
+++ script/model/sklearn_like_model/NetModule/optimizer/ProximalAdagrad.py
@@ -1,14 +1,11 @@
 import tensorflow as tf
 
-from script.model.sklearn_like_model.NetModule.BaseNetModule import BaseNetModule
-from script.model.sklearn_like_model.NetModule.DynamicVariable import DynamicVariable
+from script.model.sklearn_like_model.NetModule.optimizer.optimizer import optimizer
 
 
-class ProximalAdagrad(BaseNetModule):
+class ProximalAdagrad(optimizer):
     def __init__(
             self,
-            loss,
-            train_var_list,
             learning_rate=0.001,
             initial_accumulator_value=0.1,
             l1_regularization_strength=0.0,
@@ -17,15 +14,12 @@ class ProximalAdagrad(BaseNetModule):
             name=None,
             verbose=0
     ):
-        super().__init__(verbose=verbose, name=name, reuse=reuse)
-        self.loss = loss
-        self.train_var_list = train_var_list
+        super().__init__(learning_rate=learning_rate, verbose=verbose, name=name, reuse=reuse)
+
         self._learning_rate = learning_rate
         self._initial_accumulator_value = initial_accumulator_value
         self._l1_regularization_strength = l1_regularization_strength
         self._l2_regularization_strength = l2_regularization_strength
-
-        self._lr_module = DynamicVariable(self._learning_rate, name='learning_rate')
 
     def __str__(self):
         s = f"{self.__class__.__name__}"
@@ -36,35 +30,15 @@ class ProximalAdagrad(BaseNetModule):
 
         return s
 
-    @property
-    def learning_rate(self):
-        return self._lr_module.value
-
-    def build(self):
-        self.log.info(f'build {self.name}')
-        with tf.variable_scope(self.name):
-            self._lr_module.build()
-
-            self._optimizer = tf.train.ProximalAdagradOptimizer(
-                learning_rate=self._lr_module.variable,
-                initial_accumulator_value=self._initial_accumulator_value,
-                l1_regularization_strength=self._l1_regularization_strength,
-                l2_regularization_strength=self._l2_regularization_strength,
-            )
-            self._train_op = self.optimizer.minimize(
-                loss=self.loss,
-                var_list=self.train_var_list
-            )
-
-        return self
-
-    def update_learning_rate(self, sess, lr):
-        self._lr_module.update(sess, lr)
-
-    @property
-    def train_op(self):
+    def _build(self):
+        self._optimizer = tf.train.ProximalAdagradOptimizer(
+            learning_rate=self._lr_module.variable,
+            initial_accumulator_value=self._initial_accumulator_value,
+            l1_regularization_strength=self._l1_regularization_strength,
+            l2_regularization_strength=self._l2_regularization_strength,
+        )
+        self._train_op = self.optimizer.minimize(
+            loss=self.loss,
+            var_list=self.train_var_list
+        )
         return self._train_op
-
-    @property
-    def optimizer(self):
-        return self._optimizer
