bbb529e90a921024592df6102b99aec1112fcb31
demetoir <wnsqlehlswk@naver.com>
2019-01-28 01:12:34 +0900

    collapse dqn.py refactoring

--- script/workbench/dqn.py
+++ script/workbench/dqn.py
@@ -1,14 +1,14 @@
 import math
+import random
 from collections import deque
 from queue import Queue
-import random
-import tqdm
+
 import numpy as np
+import tqdm
 
 from script.util.Stacker import Stacker
-from script.workbench.gym_wrapper import gym_frozenlake_v0_wrapper, gym_cartpole_v1_wrapper
-
 from script.util.tensor_ops import *
+from script.workbench.gym_wrapper import gym_cartpole_v1_wrapper
 
 
 def moving_average(a, n=3):
@@ -36,25 +36,65 @@ class dqn_net:
             stack.linear(48)
             stack.tanh()
             stack.linear(self.output_size)
-            # stack.tanh()
 
             self._proba = stack.last_layer
             self._predict = tf.argmax(self._proba, axis=1)
 
-            self.Y = placeholder(tf.float32, [-1, self.output_size], 'Y')
+            self.y = placeholder(tf.float32, [-1, self.output_size], 'Y')
 
-            self.loss = MSE_loss(self._proba, self.Y, 'loss')
+            self.loss = MSE_loss(self._proba, self.y, 'loss')
             self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
 
-    def predict(self, states):
-        return self.sess.run(self._predict, feed_dict={self.x: states})
+    def predict(self, xs):
+        return self.sess.run(self._predict, feed_dict={self.x: xs})
 
-    def proba(self, states):
-        return self.sess.run(self._proba, feed_dict={self.x: states})
+    def proba(self, xs):
+        return self.sess.run(self._proba, feed_dict={self.x: xs})
 
-    def update(self, xs, ys):
-        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={self.x: xs, self.Y: ys})
+    def train(self, xs, ys):
+        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={self.x: xs, self.y: ys})
         return loss
+
+
+class replay_memory:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self._memory = deque()
+
+    def __len__(self):
+        return len(self._memory)
+
+    def sample(self, n):
+        return random.sample(self._memory, n)
+
+    def append(self, item):
+        self._memory.append(item)
+        if len(self._memory) > self.max_size:
+            self._memory.popleft()
+
+
+class moving_average_scalar:
+    def __init__(self, window_size, name='moving average'):
+        self.window_size = window_size
+        self.que = Queue(self.window_size)
+        self.name = name
+        self.windows_sum = 0.
+
+    def init_sum(self):
+        self.windows_sum = 0.
+
+    def update(self, item):
+        if self.que.full():
+            v = self.que.get()
+            self.windows_sum -= v
+            self.que.put(item)
+            self.windows_sum += item
+        else:
+            self.que.put(item)
+            self.windows_sum += item
+
+    def value(self):
+        return self.windows_sum / self.window_size
 
 
 class DQN:
@@ -70,8 +110,9 @@ class DQN:
             self.open()
 
         self._e = 1.
-
-        self.__replay_memory = deque()
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.__replay_memory = replay_memory(100000)
 
         self.main_net = dqn_net(state_space, action_space, self.lr, name='main', sess=self.sess)
         self.target_net = dqn_net(state_space, action_space, self.lr, name='target', sess=self.sess)
@@ -80,8 +121,6 @@ class DQN:
 
         init_op = tf.global_variables_initializer()
         self.sess.run(init_op)
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
 
     @property
     def e(self):
@@ -92,6 +131,10 @@ class DQN:
         val = 1.0 - math.log10((t + 1) * self.epsilon_decay)
         self._e = max(self.epsilon_min, min(float(self.e), val))
 
+    @property
+    def replay_memory(self):
+        return self.__replay_memory
+
     def open(self):
         self.sess = tf.Session()
 
@@ -100,18 +143,33 @@ class DQN:
             self.sess.close()
             self.sess = None
 
-    def update(self, batch):
+    def build_copy_weight(self):
+        src_vars = collect_vars('main')
+        dest_vars = collect_vars('target')
+
+        ops = []
+        for src, dest in zip(src_vars, dest_vars):
+            ops += [dest.assign(src.value())]
+        self.copy_ops = tf.group(ops, name='copy_network_op')
+
+    def copy_main_to_target(self):
+        self.sess.run(self.copy_ops)
+
+    def chose(self, state, env, random=True):
+        if np.random.rand(1) < self.e and random:
+            action = env.action_space.sample()
+        else:
+            action = self.main_net.predict(state)[0]
+
+        return action
+
+    def train_batch(self, batch):
         states, actions, next_states, rewards, dones = zip(*batch)
         states = np.concatenate(states)
         actions = np.array(actions)
         next_states = np.concatenate(next_states)
         rewards = np.array(rewards)
         dones = np.array(dones)
-        # print(states.shape)
-        # print(actions.shape)
-        # print(next_states.shape)
-        # print(rewards.shape)
-        # print(dones.shape)
 
         X = states
         Q_next = np.max(self.target_net.proba(next_states), 1)
@@ -122,56 +180,10 @@ class DQN:
         y[np.arange(len(X)), actions] = Q
 
         # Train our network using target and predicted Q values on each episode
-        return self.main_net.update(X, y)
-
-        # ys = []
-        # xs = []
-        # for state, action, new_state, reward, done in batch:
-        #     Q = self.main_net.proba(state)
-        #     Q_target = np.max(self.target_net.proba(new_state))
-        #
-        #     if done:
-        #         Q[0, action] = reward
-        #     else:
-        #         Q[0, action] = reward + self.discount_factor * Q_target
-        #
-        #     xs += [state]
-        #     ys += [Q]
-        #
-        # xs = np.concatenate(xs, axis=0)
-        # ys = np.concatenate(ys, axis=0)
-        #
-        # return self.main_net.update(xs, ys)
-
-    def chose(self, state, env, random=True):
-        a = self.main_net.predict(state)
-        # print(a)
-        if np.random.rand(1) < self.e and random:
-            a[0] = env.action_space.sample()
-
-        action = a[0]
-
-        return action
-
-    def build_copy_weight(self):
-        src_vars = collect_vars('main')
-        dest_vars = collect_vars('target')
-
-        ops = []
-        for src, dest in zip(src_vars, dest_vars):
-            ops += [dest.assign(src.value())]
-        self.copy_ops = ops
-
-    def copy_main_to_target(self):
-        self.sess.run(self.copy_ops)
-
-    @property
-    def replay_memory(self):
-        return self.__replay_memory
+        return self.main_net.train(X, y)
 
     def train(self, n_episodes, n_windows=10):
-        que = Queue(n_windows)
-        windows_sum = 0
+        ma_scalar = moving_average_scalar(n_windows)
 
         reward_sums = []
         for episode in tqdm.trange(n_episodes):
@@ -182,14 +194,11 @@ class DQN:
             while True:
                 action = self.chose(state, self.env)
                 next_state, reward, done, _ = self.env.step(action)
-                if done:
-                    reward = -200
 
-                # print(f'action = {action}, r = {reward}')
+                # if done:
+                #     reward = -200
 
                 self.replay_memory.append((state, action, next_state, reward, done))
-                if len(self.replay_memory) > 50000:
-                    self.replay_memory.popleft()
 
                 reward_sum += 1
 
@@ -202,31 +211,38 @@ class DQN:
             if episode % 10 == 0:
                 l_sum = 0.
                 for i in range(50):
-                    batch = random.sample(self.replay_memory, 10)
+                    batch = self.replay_memory.sample(10)
 
-                    loss = self.update(batch)
+                    loss = self.train_batch(batch)
                     l_sum += loss
                 print(f"loss sum : {l_sum / 50}")
 
                 self.copy_main_to_target()
 
             reward_sums.append(reward_sum)
-            if que.full():
-                v = que.get()
-                windows_sum -= v
-                que.put(reward_sum)
-                windows_sum += reward_sum
-            else:
-                que.put(reward_sum)
-                windows_sum += reward_sum
-            windows_average = float(windows_sum) / n_windows
-            tqdm.tqdm.write(f"moving average reward sum : {windows_average}")
+            ma_scalar.update(reward_sum)
+            # bot_play_score = self.bot_play()
+            # tqdm.tqdm.write(f"moving average reward sum : {ma_scalar.value()}, bot play score = {bot_play_score}")
+            tqdm.tqdm.write(f"moving average reward sum : {ma_scalar.value()}")
+
+            self.bot_play()
 
         return reward_sums
 
     def bot_play(self):
-        pass
+        state = self.env.reset()
+        reward_sum = 0
+        while True:
+            action = self.chose(state, self.env)
 
+            next_state, reward, done, _ = self.env.step(action)
+            reward_sum += reward
+            state = next_state
+
+            if done:
+                break
+
+        return reward_sum
 
 def dqn_cartpole():
     env = gym_cartpole_v1_wrapper()
