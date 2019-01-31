2b8347b481c78dcdd163faf25bdecec538be72a3
demetoir-desktop <wnsqlehlswk@naver.com>
2019-02-01 04:41:11 +0900

    add PER memory work

--- script/workbench/dqn.py
+++ script/workbench/dqn.py
@@ -1,6 +1,7 @@
 import math
 import random
 from collections import deque
+from pprint import pprint
 from queue import Queue
 
 import numpy as np
@@ -18,13 +19,14 @@ def moving_average(a, n=3):
 
 
 class QNetwork:
-    def __init__(self, input_size, output_size, lr, name='main', sess=None, dueling=True):
+    def __init__(self, input_size, output_size, lr, name='main', sess=None, dueling=True, PERMemory=True):
         self.input_size = input_size
         self.output_size = output_size
         self.lr = lr
         self.name = name
         self.sess = sess
         self.dueling = dueling
+        self.PERMemory = PERMemory
 
         self.build_network()
 
@@ -51,8 +53,18 @@ class QNetwork:
 
             self.y = placeholder(tf.float32, [-1, self.output_size], 'Y')
 
-            self.loss = MSE_loss(self._proba, self.y, 'loss')
-            self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
+            if self.PERMemory:
+                self.IS_weights = placeholder(tf.float32, [-1, ], name='IS_weights')
+                loss = tf.reduce_mean((self._proba - self.y) * (self._proba - self.y), axis=1)
+
+                self.loss = identity(self.IS_weights * loss, name='loss')
+                self.loss_mean = tf.reduce_mean(self.loss, name='loss_mean')
+
+                self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss_mean)
+            else:
+                self.loss = MSE_loss(self._proba, self.y, 'loss')
+
+                self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
 
     def predict(self, xs):
         return self.sess.run(self._predict, feed_dict={self.x: xs})
@@ -60,8 +72,24 @@ class QNetwork:
     def proba(self, xs):
         return self.sess.run(self._proba, feed_dict={self.x: xs})
 
-    def train(self, xs, ys):
-        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={self.x: xs, self.y: ys})
+    def train(self, xs, ys, IS_weights=None):
+        if self.PERMemory:
+            loss, _ = self.sess.run(
+                [self.loss, self.train_op],
+                feed_dict={
+                    self.x: xs,
+                    self.y: ys,
+                    self.IS_weights: IS_weights,
+                }
+            )
+        else:
+            loss, _ = self.sess.run(
+                [self.loss, self.train_op],
+                feed_dict={
+                    self.x: xs,
+                    self.y: ys,
+                }
+            )
         return loss
 
     def loss(self, xs, ys):
@@ -79,10 +107,227 @@ class ReplayMemory:
     def sample(self, n):
         return random.sample(self._memory, n)
 
-    def append(self, item):
+    def store(self, item):
         self._memory.append(item)
         if len(self._memory) > self.max_size:
             self._memory.popleft()
+
+
+class SumTree(object):
+    data_pointer = 0
+
+    def __init__(self, capacity):
+        self.capacity = capacity  # Number of leaf nodes (final nodes) that contains experiences
+
+        self.tree = np.zeros(2 * capacity - 1)
+
+        # Contains the experiences (so the size of data is capacity)
+        self.data = np.zeros(capacity, dtype=object)
+
+    def add(self, priority, data):
+        # Look at what index we want to put the experience
+        tree_index = self.data_pointer + self.capacity - 1
+
+        # Update data frame
+        self.data[self.data_pointer] = data
+
+        # Update the leaf
+        self.update(tree_index, priority)
+
+        # Add 1 to data_pointer
+        self.data_pointer += 1
+        if self.data_pointer >= self.capacity:
+            self.data_pointer = 0
+
+        # self.data_pointer = (self.data_pointer + 1) % self.leaf_size
+
+    def update(self, tree_index, priority):
+        # Change = new priority score - former priority score
+        diff = priority - self.tree[tree_index]
+        self.tree[tree_index] = priority
+
+        # then propagate the diff through tree
+        while tree_index != 0:  # this method is faster than the recursive loop in the reference code
+            tree_index = (tree_index - 1) // 2
+            self.tree[tree_index] += diff
+
+    def get_leaf(self, v):
+        parent_index = 0
+        while True:  # the while loop is faster than the method in the reference code
+            left_child_index = 2 * parent_index + 1
+            right_child_index = left_child_index + 1
+
+            # If we reach bottom, end the search
+            if left_child_index >= len(self.tree):
+                leaf_index = parent_index
+                break
+
+            if v <= self.tree[left_child_index]:
+                parent_index = left_child_index
+            else:
+                v -= self.tree[left_child_index]
+                parent_index = right_child_index
+
+        data_index = leaf_index - self.capacity + 1
+
+        return leaf_index, self.tree[leaf_index], self.data[data_index]
+
+    @property
+    def total_priority(self):
+        return self.tree[0]
+
+
+class PERMemory(object):  # stored as ( s, a, r, s_ ) in SumTree
+    PER_e = 0.01  # Hyperparameter that we use to avoid some experiences to have 0 probability of being taken
+    PER_a = 0.6  # Hyperparameter that we use to make a tradeoff between taking only exp with high priority and sampling randomly
+    PER_b = 0.4  # importance-sampling, from initial value increasing to 1
+
+    PER_b_increment_per_sampling = 0.001
+
+    absolute_error_upper = 1.  # clipped abs error
+
+    def __init__(self, capacity, e=0.01, a=0.6, b=0.4):
+        self.e = e
+        self.a = a
+        self.b = b
+        self.tree = SumTree(capacity)
+        self.cnt = 0
+
+    @property
+    def capacity(self):
+        return self.tree.capacity
+
+    def store(self, experience):
+        # Find the max priority
+        max_priority = np.max(self.tree.tree[-self.tree.capacity:])
+
+        # If the max priority = 0 we can't put priority = 0 since this exp will never have a chance to be selected
+        # So we use a minimum priority
+        if max_priority == 0:
+            max_priority = self.absolute_error_upper
+
+        self.tree.add(max_priority, experience)  # set the max p for new p
+
+        self.cnt = min(self.cnt + 1, self.capacity)
+
+    def __len__(self):
+        return self.cnt
+
+    def sample(self, n):
+        # Create a sample array that will contains the minibatch
+        memory_b = []
+
+        b_idx, b_ISWeights = np.empty((n,), dtype=np.int32), np.empty((n, 1), dtype=np.float32)
+
+        # Calculate the priority segment
+        # Here, as explained in the paper, we divide the Range[0, ptotal] into n ranges
+        priority_segment = self.tree.total_priority / n  # priority segment
+
+        # Here we increasing the PER_b each time we sample a new minibatch
+        self.PER_b = np.min([1., self.PER_b + self.PER_b_increment_per_sampling])  # max = 1
+
+        # Calculating the max_weight
+        p_min = np.min(self.tree.tree[-self.tree.capacity:]) / self.tree.total_priority
+        # p_min = np.max(self.tree.tree[-self.tree.capacity:]) / self.tree.total_priority
+        # print(self.tree.total_priority)
+        # max_weight = (p_min * n) ** (-self.PER_b)
+
+        for i in range(n):
+            """
+            A value is uniformly sample from each range
+            """
+            a, b = priority_segment * i, priority_segment * (i + 1)
+            value = np.random.uniform(a, b)
+
+            """
+            Experience that correspond to each value is retrieved
+            """
+            index, priority, data = self.tree.get_leaf(value)
+
+            # P(j)
+            sampling_probabilities = priority / self.tree.total_priority
+
+            #  IS = (1/N * 1/P(i))**b /max wi == (N*P(i))**-b  /max wi
+            # b_ISWeights[i, 0] = np.power(n * sampling_probabilities, -self.PER_b) / max_weight
+            b_ISWeights[i, 0] = np.power((sampling_probabilities * self.capacity), -self.PER_b)
+
+
+            b_idx[i] = index
+
+            experience = [data]
+
+            memory_b.append(experience)
+
+        memory_b = np.array(memory_b).reshape([32, 5])
+        b_ISWeights = np.array(b_ISWeights).reshape([-1])
+        b_ISWeights /= np.max(b_ISWeights)
+        # print(b_ISWeights)
+        return b_idx, memory_b, b_ISWeights
+
+    def batch_update(self, tree_idx, abs_errors):
+        abs_errors += self.PER_e  # convert to abs and avoid 0
+        clipped_errors = np.minimum(abs_errors, self.absolute_error_upper)
+        ps = np.power(clipped_errors, self.PER_a)
+
+        for ti, p in zip(tree_idx, ps):
+            self.tree.update(ti, p)
+
+
+# def full_with_memory():
+#     # Instantiate memory
+#     memory = PERMemory(memory_size)
+#
+#     # Render the environment
+#     game.new_episode()
+#
+#     for i in range(pretrain_length):
+#         # If it's the first step
+#         if i == 0:
+#             # First we need a state
+#             state = game.get_state().screen_buffer
+#             state, stacked_frames = stack_frames(stacked_frames, state, True)
+#
+#         # Random action
+#         action = random.choice(possible_actions)
+#
+#         # Get the rewards
+#         reward = game.make_action(action)
+#
+#         # Look if the episode is finished
+#         done = game.is_episode_finished()
+#
+#         # If we're dead
+#         if done:
+#             # We finished the episode
+#             next_state = np.zeros(state.shape)
+#
+#             # Add experience to memory
+#             # experience = np.hstack((state, [action, reward], next_state, done))
+#
+#             experience = state, action, reward, next_state, done
+#             memory.store(experience)
+#
+#             # Start a new episode
+#             game.new_episode()
+#
+#             # First we need a state
+#             state = game.get_state().screen_buffer
+#
+#             # Stack the frames
+#             state, stacked_frames = stack_frames(stacked_frames, state, True)
+#
+#         else:
+#             # Get the next state
+#             next_state = game.get_state().screen_buffer
+#             next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+#
+#             # Add experience to memory
+#             experience = state, action, reward, next_state, done
+#             memory.store(experience)
+#
+#             # Our state is now the next_state
+#             state = next_state
+#     pass
 
 
 class moving_average_scalar:
@@ -110,7 +355,8 @@ class moving_average_scalar:
 
 
 class DQN:
-    def __init__(self, env, lr, discount_factor, state_space, action_space, sess=None, double=True, dueling=True):
+    def __init__(self, env, lr, discount_factor, state_space, action_space, sess=None, double=True, dueling=True,
+                 replay_memory_size=100000, PER=True):
         self.env = env
         self.lr = lr
         self.discount_factor = discount_factor
@@ -124,7 +370,13 @@ class DQN:
         self._e = 1.
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.__replay_memory = ReplayMemory(100000)
+
+        self.PER = PER
+        self.replay_memory_size = replay_memory_size
+        if self.PER:
+            self.__replay_memory = PERMemory(self.replay_memory_size)
+        else:
+            self.__replay_memory = ReplayMemory(self.replay_memory_size)
 
         self.double = double
         self.dueling = dueling
@@ -181,7 +433,7 @@ class DQN:
 
         return action
 
-    def train_batch(self, batch):
+    def train_batch(self, batch, IS_weight=None):
         states, actions, next_states, rewards, dones = zip(*batch)
         states = np.concatenate(states)
         actions = np.array(actions)
@@ -205,7 +457,13 @@ class DQN:
         y[np.arange(len(X)), actions] = Q
 
         # Train our network using target and predicted Q values on each episode
-        return self.main_Qnet.train(X, y)
+
+        if self.PER:
+            loss = self.main_Qnet.train(X, y, IS_weight)
+        else:
+            loss = self.main_Qnet.train(X, y)
+
+        return loss
 
     def train(self, n_episodes, n_windows=10):
         ma_scalar = moving_average_scalar(n_windows)
@@ -220,10 +478,7 @@ class DQN:
                 action = self.chose(state, self.env)
                 next_state, reward, done, _ = self.env.step(action)
 
-                # if done:
-                #     reward = -200
-
-                self.replay_memory.append((state, action, next_state, reward, done))
+                self.replay_memory.store((state, action, next_state, reward, done))
 
                 reward_sum += 1
 
@@ -236,9 +491,14 @@ class DQN:
             if episode % self.train_interval == 0 and len(self.replay_memory) >= self.batch_size:
                 l_sum = 0.
                 for i in range(50):
-                    batch = self.replay_memory.sample(self.batch_size)
+                    if self.PER:
+                        tree_idx, batch, IS_weights = self.replay_memory.sample(self.batch_size)
 
-                    loss = self.train_batch(batch)
+                        loss = self.train_batch(batch, IS_weights)
+                        self.replay_memory.batch_update(tree_idx, loss)
+                    else:
+                        batch = self.replay_memory.sample(self.batch_size)
+                        loss = self.train_batch(batch)
                     l_sum += loss
                 tqdm.tqdm.write(f"loss: {l_sum / 50}")
 
@@ -249,7 +509,7 @@ class DQN:
             ma_scalar.update(reward_sum)
             # bot_play_score = self.bot_play()
             # tqdm.tqdm.write(f"moving average reward sum : {ma_scalar.value()}, bot play score = {bot_play_score}")
-            tqdm.tqdm.write(f"moving average reward sum : {ma_scalar.value()}")
+            tqdm.tqdm.write(f"moving average reward sum : {ma_scalar.value()}, max score = {max(reward_sums)}")
 
             self.bot_play()
 
@@ -281,7 +541,10 @@ def dqn_cartpole():
     discount_factor = 0.99
     max_episodes = 3000
     # max_episodes = 30
-    dqn = DQN(env, lr, discount_factor, observation_space, action_space)
+    dueling = True
+    double = True
+    PER = True
+    dqn = DQN(env, lr, discount_factor, observation_space, action_space, double=double, dueling=dueling, PER=PER)
     reward_sums = dqn.train(max_episodes)
 
     reward_sums = moving_average(reward_sums, 100)
